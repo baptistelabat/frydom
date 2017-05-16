7c66c883d96e97f669f5e829d0ead93414b03a5c
François Rongère <Francois.Rongere@ec-nantes.fr>
2017-05-16 17:20:08 +0200

    Creating a chrono python module to get chrono C++ lib importable into frydom

---
+++ frydom/core/chrono.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+import os
+import sys
+
+__all__ = ['engine', 'fea', 'irrlicht', 'postprocessing']
+
+
+class ImportChronoError(Exception):
+    pass
+
+
+_dir_here = os.path.abspath(os.path.dirname(__file__))
+_chrono_python_modules = os.path.join(_dir_here, 'chrono', 'build', 'bin')
+
+sys.path.insert(0, _chrono_python_modules)
+
+try:
+    import ChronoEngine_python_core as engine
+except ImportError:
+    raise ImportChronoError('ChronoEngine core is not available and should be compiled with python support')
+
+try:
+    import ChronoEngine_python_fea as fea
+except ImportError:
+    raise ImportChronoError('ChronoEngine finite element module is not available and should be compiled with python '
+                            'support')
+
+try:
+    import ChronoEngine_python_irrlicht as irrlicht
+except ImportError:
+    raise ImportChronoError('ChronoEngine irrlicht visualization support is not available and should be compiled with '
+                            'python support')
+
+try:
+    import ChronoEngine_python_postprocess as postprocessing
+except ImportError:
+    raise ImportChronoError('ChronoEngine post-processing facilities is not available and should be compiled with '
+                            'python support')
---
+++ tests/core/chrono/test_import_chrono.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+def test_import_chrono_all():
+    import frydom.core.chrono as chrono
