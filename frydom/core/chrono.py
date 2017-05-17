27ea8d5f49474c7275be5e5d305bee7e50992d37
François Rongère <Francois.Rongere@ec-nantes.fr>
2017-05-17 13:35:12 +0200

    Little refactoring

--- frydom/core/chrono.py
+++ frydom/core/chrono.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
 
-__all__ = ['engine', 'fea', 'irrlicht', 'postprocessing']
+__all__ = ['core', 'fea', 'irrlicht', 'postprocess']
 
 import os
 import sys
 
 
-class ImportChronoError(Exception):
+class ChronoImportError(Exception):
     pass
 
 
@@ -16,25 +16,24 @@ _chrono_python_modules = os.path.join(_dir_here, 'chrono', 'build', 'bin')
 
 sys.path.insert(0, _chrono_python_modules)
 
+_err_msg = "%s is not available and must be compiled with python support"
+
 try:
-    import ChronoEngine_python_core as engine
+    import ChronoEngine_python_core as core
 except ImportError:
-    raise ImportChronoError('ChronoEngine core is not available and should be compiled with python support')
+    raise ChronoImportError(_err_msg % 'ChronoEngine_python_core')
 
 try:
     import ChronoEngine_python_fea as fea
 except ImportError:
-    raise ImportChronoError('ChronoEngine finite element module is not available and should be compiled with python '
-                            'support')
+    raise ChronoImportError(_err_msg % 'ChronoEngine_python_fea')
 
 try:
     import ChronoEngine_python_irrlicht as irrlicht
 except ImportError:
-    raise ImportChronoError('ChronoEngine irrlicht visualization support is not available and should be compiled with '
-                            'python support')
+    raise ChronoImportError(_err_msg % 'ChronoEngine_python_irrlicht')
 
 try:
-    import ChronoEngine_python_postprocess as postprocessing
+    import ChronoEngine_python_postprocess as postprocess
 except ImportError:
-    raise ImportChronoError('ChronoEngine post-processing facilities is not available and should be compiled with '
-                            'python support')
+    raise ChronoImportError(_err_msg % 'ChronoEngine_python_postprocess')
--- tests/core/chrono/test_import_chrono.py
+++ tests/core/chrono/test_import_chrono.py
@@ -3,3 +3,7 @@
 
 def test_import_chrono_all():
     import frydom.core.chrono as chrono
+
+
+if __name__ == '__main__':
+    test_import_chrono_all()
