646ee3ef7e3b675ca237bb8005e8ebf5cbdbf282
François Rongère <Francois.Rongere@ec-nantes.fr>
2017-05-31 10:03:32 +0200

    WIP

--- src/core/FrOffshoreSystem.cpp
+++ src/core/FrOffshoreSystem.cpp
@@ -9,9 +9,19 @@ namespace frydom {
 
     FrOffshoreSystem::FrOffshoreSystem() {
 
-        // Creating a default flat free surface that may be changed
+        // Creating the first pointer to this
+//        auto ptr = std::make_shared<FrOffshoreSystem>(this);
+//        shared_from_this();
+
+
+        // Creating a default flat free surface
+
         environment::FrFlatFreeSurface freeSurface(this, 2.);
-        m_free_surface.reset(&freeSurface);
+//        m_free_surface = environment::FrFlatFreeSurface(this, 2.);
+//        environment::FrFlatFreeSurface* fs_ptr = &freeSurface;
+        environment::FrFlatFreeSurface* fs_ptr = &freeSurface;
+        m_free_surface.reset(fs_ptr);
+
 
     }
 
--- src/core/FrOffshoreSystem.h
+++ src/core/FrOffshoreSystem.h
@@ -10,29 +10,32 @@
 
 namespace frydom {
 
-//    class FrFreeSurface;  // forward declaration
+        class FrFreeSurface;  // forward declaration
 
-    // TODO: voir aussi a deriver de ChSystemSMC pour comparer les 2 ? Avoir une classe de base virtuelle derivant de ChSystem ???
-    class FrOffshoreSystem : public chrono::ChSystemNSC {
+        // TODO: voir aussi a deriver de ChSystemSMC pour comparer les 2 ? Avoir une classe de base virtuelle derivant de ChSystem ???
+        class FrOffshoreSystem : public chrono::ChSystemNSC {
+//        class FrOffshoreSystem : public chrono::ChSystemNSC, public std::enable_shared_from_this<FrOffshoreSystem> {
 
-      public:
-        /// Default constructor
-        FrOffshoreSystem();
+        public:
+            /// Default constructor
+            FrOffshoreSystem();
 
-        /// Default destructor
-        ~FrOffshoreSystem() {};
+            /// Default destructor
+            ~FrOffshoreSystem() {};
 
-        /// Get the wave field model from the offshore system.
-//        std::unique_ptr<environment::FrFreeSurface> getFreeSurface();
+            /// Add a free surface model to the system
+            void addFreeSurface(std::unique_ptr<environment::FrFreeSurface>) {};
 
-//        /// Get a shared pointer from the system
-//        std::shared_ptr<FrOffshoreSystem> getShared(void);
+            /// Get the free surface model from the offshore system.
+            std::unique_ptr<environment::FrFreeSurface> getFreeSurface() { return 0; };
 
-      private:
-        /// The free surface's mesh that is a cartesian grid.
-        std::unique_ptr<environment::FrFreeSurface> m_free_surface;
+        private:
+            /// The free surface's mesh that is a cartesian grid.
+            std::unique_ptr<environment::FrFreeSurface> m_free_surface;
 
-    };
+            /// A shared pointer to this
+//            std::shared_ptr<FrOffshoreSystem> m_ptr_to_this;
+        };
 
 } // end namespace frydom
 
