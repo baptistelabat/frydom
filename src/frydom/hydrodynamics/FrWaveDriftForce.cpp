0dc6aa110d78eb9d10870c9c84593f26c614628c
Camille Chauvigne <camille.chauvigne@dice-engineering.com>
2017-12-19 16:43:16 +0100

    WIP
    Conflicts:
    	CMakeLists.txt
    	thirdparty/MathUtils

--- src/frydom/hydrodynamics/FrWaveDriftForce.cpp
+++ src/frydom/hydrodynamics/FrWaveDriftForce.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "FrWaveDriftForce.h"
+
+
+namespace frydom {
+
+    FrWaveDriftForce::FrWaveDriftForce(const std::string yaml_file) {
+
+    }
+
+    void FrWaveDriftForce::SetCmplxElevation() {
+
+        auto x = m_waveProbe->GetX();
+        auto y = m_waveProbe->GetY();
+        m_CmplxElevation = m_waveProbe->GetWaveField()->GetCmplxElevation(x, y);
+
+    }
+
+    void FrWaveDriftForce::UpdateState() {
+
+        std::vector<double> drift_coeff(3);
+
+        auto heading = m_body->GetHeadingAngle(NED);
+
+        auto emjwt = m_waveProbe->GetWaveField()->GetTimeCoeffs();
+
+        std::vector<std::complex<double>> cforce = {0.,0.,0.};
+
+        for (unsigned int ifreq=0; ifreq<emjwt.size(); ++ifreq) {
+
+            drift_coeff = { m_table.Eval("Surge", wi, heading),
+                            m_table.Eval("Sway", wi, heading),
+                            m_table.Eval("Yaw", wi, heading) };
+
+            cforce += m_CmplxElevation[ifreq] .* drift_coeff * emjwt[ifreq];
+        }
+        force.at(0) = std::real(cforce.at(0));   //  Surge
+        force.at(1) = std::real(cforce.at(1));   //  Sway
+        moment.at(3) = std::real(cforce.at(2));  //  Yaw
+     }
+
+
+
+
+}
--- src/frydom/hydrodynamics/FrWaveDriftForce.h
+++ src/frydom/hydrodynamics/FrWaveDriftForce.h
@@ -7,6 +7,7 @@
 
 #include <frydom/core/FrForce.h>
 #include <frydom/environment/waves/FrWaveProbe.h>
+#include <frydom/core/FrHydroBody.h>
 
 namespace frydom {
 
@@ -14,8 +15,17 @@ namespace frydom {
 
     public:
 
+        /// Construct a new force model from drift table coefficients
+        FrWaveDriftForce(const std::string yaml_file);
+
         /// Wave probe attached to the force at the application point location
         void SetWaveProbe(std::shared_ptr<FrLinearWaveProbe>& waveProbe) { m_waveProbe = waveProbe; }
+
+        /// Definition of the complex amplitude at the wave probe
+        void SetCmplxElevation();
+
+        /// Definition of the body where the force is applied
+        void SetBody(FrHydroBody* body) { m_body = body;}
 
         /// Update procedure containing the Wave Drift Force definition
         void UpdateState() override;
@@ -23,7 +33,9 @@ namespace frydom {
     private:
 
         std::shared_ptr<FrLinearWaveProbe> m_waveProbe;    ///< Wave probe for local wave field characteristics
-
+        std::vector<std::vector<std::complex<double>>> m_CmplxElevation;  ///< Wave complex elevation
+        LookupTable2d<> m_table;                           ///< Table of the wave drift force depending on freq. and dir.
+        FrHydroBody*    m_body;                            ///< Hydro body to which the force is applied
 
     };
 
