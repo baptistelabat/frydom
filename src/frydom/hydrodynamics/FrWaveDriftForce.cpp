ad80c315968a23fbe40c081f54306612e1598df6
Camille Chauvigne <camille.chauvigne@dice-engineering.com>
2017-12-20 12:01:01 +0100

    Definition of the wave drift force computation

--- src/frydom/hydrodynamics/FrWaveDriftForce.cpp
+++ src/frydom/hydrodynamics/FrWaveDriftForce.cpp
@@ -2,6 +2,7 @@
 // Created by camille on 14/12/17.
 //
 
+#include "frydom/environment/waves/FrWaveField.h"
 #include "FrWaveDriftForce.h"
 
 
@@ -15,13 +16,17 @@ namespace frydom {
 
         auto x = m_waveProbe->GetX();
         auto y = m_waveProbe->GetY();
-        m_CmplxElevation = m_waveProbe->GetWaveField()->GetCmplxElevation(x, y);
+
+        m_CmplxElevation = m_waveProbe->GetWaveField()->GetCmplxElevation(x, y, true);
 
     }
 
     void FrWaveDriftForce::UpdateState() {
 
-        std::vector<double> drift_coeff(3);
+        //std::vector<double> drift_coeff(3); //FIXME : dynamic behaviour
+
+        auto nbFreq = m_waveProbe->GetWaveField()->GetNbFrequencies();
+        auto nbWaveDir = m_waveProbe->GetWaveField()->GetNbWaveDirections();
 
         auto heading = m_body->GetHeadingAngle(NED);
 
@@ -29,17 +34,31 @@ namespace frydom {
 
         std::vector<std::complex<double>> cforce = {0.,0.,0.};
 
-        for (unsigned int ifreq=0; ifreq<emjwt.size(); ++ifreq) {
+        auto waveDir = m_waveProbe->GetWaveField()->GetWaveDirections(RAD);
+        auto w = m_waveProbe->GetWaveField()->GetWaveFrequencies(RADS);
+        double wi, relative_angle;
 
-            drift_coeff = { m_table.Eval("Surge", wi, heading),
-                            m_table.Eval("Sway", wi, heading),
-                            m_table.Eval("Yaw", wi, heading) };
+        for (unsigned int idir=0; idir<nbWaveDir; ++idir) {
+            for (unsigned int ifreq=0; ifreq < nbFreq; ++ifreq) {
 
-            cforce += m_CmplxElevation[ifreq] .* drift_coeff * emjwt[ifreq];
+                wi  =w[ifreq];
+                relative_angle = heading - waveDir[idir];
+
+                //drift_coeff = {m_table.Eval("Surge", wi, relative_angle),
+                //               m_table.Eval("Sway", wi, relative_angle),
+               //               m_table.Eval("Yaw", wi, relative_angle)};
+
+                //cforce += m_CmplxElevation[idir][ifreq] .* drift_coeff * emjwt[ifreq];
+
+                cforce.at(0) += m_CmplxElevation[idir][ifreq] * m_table.Eval("Surge", wi, relative_angle) * emjwt[ifreq];
+                cforce.at(1) += m_CmplxElevation[idir][ifreq] * m_table.Eval("Sway", wi, relative_angle) * emjwt[ifreq];
+                cforce.at(2) += m_CmplxElevation[idir][ifreq] * m_table.Eval("Yaw", wi, relative_angle) * emjwt[ifreq];
+
+            }
         }
-        force.at(0) = std::real(cforce.at(0));   //  Surge
-        force.at(1) = std::real(cforce.at(1));   //  Sway
-        moment.at(3) = std::real(cforce.at(2));  //  Yaw
+        force.x() = std::real(cforce.at(0));   //  Surge
+        force.y() = std::real(cforce.at(1));   //  Sway
+        moment.z() = std::real(cforce.at(2));  //  Yaw
      }
 
 
