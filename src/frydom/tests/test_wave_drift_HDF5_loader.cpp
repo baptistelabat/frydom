37be6049e2da42bab6674b5906580cc2e9686e73
Camille Chauvigne <camille.chauvigne@dice-engineering.com>
2017-12-22 11:31:04 +0100

    Fix CMakeList automatic merge
    Conflicts:
    	CMakeLists.txt
    	src/frydom/tests/CMakeLists.txt

---
+++ src/frydom/tests/test_wave_drift_HDF5_loader.cpp
@@ -0,0 +1,14 @@
+//
+// Created by camille on 21/12/17.
+//
+
+#include <frydom/frydom.h>
+#include <frydom/hydrodynamics/FrWaveDriftForce.h>
+
+using namespace frydom;
+
+int main(int argc, char* argv[]) {
+
+
+
+}
