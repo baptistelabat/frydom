8b95f6a1b6462d55f301df8af1d49ff23159906c
François Rongère <Francois.Rongere@ec-nantes.fr>
2018-03-02 18:38:51 +0100

    Validating good part of the new orientation features
    Conflicts:
    	CMakeLists.txt
    
    Validating good part of the new orientation features

--- src/tests/demo_rotation.cpp
+++ src/tests/demo_rotation.cpp
@@ -18,10 +18,9 @@ int main(int argc, char* argv[]) {
 //    // A facon chrono
 
     ChMatrix33<> mat1;
-    double deg = M_DEG;
-    mat1.Set_A_Cardano(cardan_angles * deg);
+    mat1.Set_A_Cardano(radians(cardan_angles));
     auto quat1 = mat1.Get_A_quaternion();
-    auto cardan_angles2 = mat1.Get_A_Cardano() / deg;
+    auto cardan_angles2 = degrees(mat1.Get_A_Cardano());
 
     // A facon frydom
     auto mat2 = euler_to_mat(cardan_angles[0], cardan_angles[1], cardan_angles[2], CARDAN, DEG);
@@ -29,21 +28,39 @@ int main(int argc, char* argv[]) {
 //    auto quat2 = mat2.Get_A_quaternion();
 
     auto quat2 = euler_to_quat(cardan_angles[0], cardan_angles[1], cardan_angles[2], CARDAN, DEG);
+    auto mat3 = quat_to_mat(quat2);
+    auto quat3 = mat_to_quat(mat2);
 
-    auto quat2inv = quat2.GetInverse();
-    auto card = quat_to_euler(quat2inv, CARDAN, RAD);
+    auto card_rec = quat_to_euler(quat2, CARDAN, DEG);
 
-    auto mat3 = euler_to_mat(card, CARDAN, RAD);
-    mat3.FastInvert(mat3);
-    auto q = mat_to_quat(mat3);
-    auto c = quat_to_euler(q, CARDAN, DEG);
+    chrono::ChVector<> axis;
+    double angle;
 
+    quat_to_axis_angle(quat3, axis, angle, DEG);
 
 
 
-    auto angles = frydom::mat_to_euler(mat2, frydom::CARDAN, frydom::DEG);
+    auto quatz = axis_angle_to_quat(chrono::VECT_Z, 30, DEG);
+    auto quaty = axis_angle_to_quat(chrono::VECT_Y, 20, DEG);
+    auto quatx = axis_angle_to_quat(chrono::VECT_X, 10, DEG);
 
-    auto angles2 = frydom::quat_to_euler(quat2, frydom::CARDAN, frydom::DEG);
+    auto quat_total = quatx * quaty * quatz;
+    auto quat_total1 = quatz * quaty * quatx;
+
+
+
+
+
+//    auto quat2inv = quat2.GetInverse();
+//    auto card = quat_to_euler(quat2inv, CARDAN, RAD);
+//
+//    auto mat3 = euler_to_mat(card, CARDAN, RAD);
+//    mat3.FastInvert(mat3);
+//    auto q = mat_to_quat(mat3);
+//    auto c = quat_to_euler(q, CARDAN, DEG);
+
+
+
 
     return 0;
 
