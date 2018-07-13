e8068c73e66373529ce316d3954c1e6a4803a791
Letournel <lucas.letournel@dice-engineering.com>
2018-07-13 10:56:37 +0200

    [WIP] new methods implemented to get the wind relative velocity, those for the current were modified to get local or global results.

---
+++ tests/test_current-wind_relative_velocity.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Lucas Letournel on 13/07/18.
+//
+
+#include <chrono/core/ChVector.h>
+#include <frydom/frydom.h>
+
+using namespace frydom;
+int main() {
+    FrOffshoreSystem system;
+
+    /// Set the current properties
+    system.GetEnvironment()->GetCurrent()->Set(NORTH, 10, MS, NED, GOTO);
+    system.GetEnvironment()->GetWind()->Set(NORTH, 10, MS, NED, GOTO);
+
+    /// Create a Ship
+    auto ship = std::make_shared<FrShip>();
+    /// Ship initial position and orientation
+    ship->SetPos(chrono::ChVector<>(0., 0., 0.));
+    ship->SetNEDHeading(EAST);
+    /// Ship initial velocity
+    auto ship_velocity = ship->TransformDirectionLocalToParent(chrono::ChVector<>(25., 0., 0.));
+    ship->SetPos_dt(ship_velocity);
+    /// Set Rate of Turn
+    ship->SetWvel_loc(chrono::ChVector<>(0,0,0));
+
+    auto Node = ship->CreateNode(chrono::ChVector<>(10,0,0));
+
+    /// Add the Ship to the Offshore System
+    system.AddBody(ship);
+    ship->Initialize();
+    ship->Update();
+
+    auto currentRelativeVelocity = ship->GetCurrentRelativeVelocity(chrono::ChVector<>(10,0,0),NED,PARENT);
+    auto windRelativeVelocity = ship->GetWindRelativeVelocity(Node.get(),NED,PARENT);
+
+    fmt::print("Current relative velocity : ({},{},{})\n",
+               currentRelativeVelocity.x(),currentRelativeVelocity.y(),currentRelativeVelocity.z());
+    fmt::print("Wind relative velocity    : ({},{},{})\n",
+               windRelativeVelocity.x(),windRelativeVelocity.y(),windRelativeVelocity.z());
+
+
+    return 0 ;
+}
