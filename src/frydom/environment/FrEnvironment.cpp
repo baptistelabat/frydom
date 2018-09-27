1ab604fc14f9916e5c4fa00e978fba9f0c007db8
Letournel <lucas.letournel@dice-engineering.com>
2018-09-27 10:09:12 +0200

    [BUGFIX] back to old version of FrEnvironment without wrap to GeoLib : memory leak to be fixed with it.

--- src/frydom/environment/FrEnvironment.cpp
+++ src/frydom/environment/FrEnvironment.cpp
@@ -14,12 +14,23 @@ namespace frydom {
         m_system->AddBody(m_freeSurface->GetBody());
     }
 
-
-
     int FrEnvironment::GetYear() {
         /// Get the UTC time to obtain the year
         auto lt = GetTimeZone()->GetUTCTime();
         date::year_month_day ymd{date::floor<date::days>(lt)};
         return int(ymd.year());
+    }
+
+    double FrEnvironment::ComputeMagneticDeclination(double x, double y, double z) {
+        /// Magnetic model loaded from _deps directory
+        GeographicLib::MagneticModel magneticModel("emm2017", "../_deps/magneticmodel-src");
+        double lat, lon, h;
+        /// Convert the node local coordinates to geographical coordinates
+        Convert_CartToGeo(x, y, z, lat, lon, h);
+        /// Compute the magnetic declination
+        double Bx, By, Bz, H, F, D, I;
+        magneticModel(GetYear(), lat, lon, h, Bx, By, Bz);
+        GeographicLib::MagneticModel::FieldComponents(Bx, By, Bz, H, F, D, I);
+        return D;
     }
 }
