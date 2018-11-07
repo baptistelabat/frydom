1f78869c9fc292bc02bb806cee00650dd5327f41
Lucas Letournel <lucas.letournel@dice-engineering.com>
2018-11-07 10:46:16 +0100

    [WIP] test on orientation, bug in Chrono reported.

--- tests/GoogleTest/test_FrBody_.cpp
+++ tests/GoogleTest/test_FrBody_.cpp
@@ -12,7 +12,7 @@ using namespace frydom;
 class TestFrBody_ :public FrBody_ {
 public:
     void TestSettersGettersPosition(){
-        Position OrigAbsPos(1.,2,.3);
+        Position OrigAbsPos(1.,2.,3.);
         SetAbsPosition(OrigAbsPos,NWU);
 
         // Test of the getter for the absolute position (expressed in the world reference frame)
@@ -35,7 +35,7 @@ public:
 
         //-----------------Fixed Point-----------------//
         // Test for the getter for the local position of a point expressed in the world reference frame
-        Position OrigAbsPosLocalPoint(4.,5,.6);
+        Position OrigAbsPosLocalPoint(4.,5.,6.);
         testPosition = GetLocalPositionOfAbsPoint(OrigAbsPosLocalPoint,NWU)-(OrigAbsPosLocalPoint - OrigAbsPos);
         EXPECT_TRUE(testPosition.isZero());
 
@@ -44,7 +44,6 @@ public:
         // Set a new orientation for the body, expressed using CARDAN angles, in the world reference frame)
         FrRotation_ OrigAbsRot; OrigAbsRot.SetCardanAngles_DEGREES(1.,2.,3.,NWU);
         SetAbsRotation(OrigAbsRot);
-        SetCardanAngles_DEGREES(1.,2.,3.,NWU);
 
         // Test of the getter for the absolute orientation (expressed in the world reference frame)
         EXPECT_TRUE(OrigAbsRot==GetAbsRotation());
@@ -54,17 +53,30 @@ public:
         EXPECT_TRUE(OrigAbsRot==GetAbsRotation());
 
         // Rotation to an easy transformation
-        SetCardanAngles_DEGREES(90.,0.,0.,NWU);
+        FrRotation_ Rotation1; Rotation1.SetCardanAngles_DEGREES(90.,0.,0.,NWU);
+        FrRotation_ Rotation2; Rotation2.SetCardanAngles_DEGREES(0.,0*90.,0.,NWU);
+        FrRotation_ Rotation3; Rotation3.SetCardanAngles_DEGREES(0.,0.,0*90.,NWU);
+        FrRotation_ TotalRotation = Rotation2*Rotation1; // *Rotation2*Rotation3;
+        double q0, q1, q2, q3; TotalRotation.GetQuaternion().Get(q0,q1,q2,q3,NWU);
+        std::cout<<"Quaternion : q0 = "<<q0<<", q1 = "<<q1<<", q2 = "<<q2<<", q3 = "<<q3<<std::endl;
+        std::cout<<TotalRotation<<std::endl;
+        SetAbsRotation(TotalRotation);
+
+//        SetCardanAngles_DEGREES(90.,0.,0.,NWU);
 
         // Test that the orientation of the body changed the previous getter result
         Position OrigPosTest = (OrigAbsPosLocalPoint - OrigAbsPos);
         auto OrigPosTestX = OrigPosTest.GetX();
-        auto OrigPosTestY = OrigPosTest.GetY();
-        auto OrigPosTestZ = OrigPosTest.GetZ();
-        Position NewOrigPos (OrigPosTestX, -OrigPosTestZ, OrigPosTestY);
+        auto OrigPosTestY = -OrigPosTest.GetZ();
+        auto OrigPosTestZ = OrigPosTest.GetY();
+//        auto OrigPosTestX = OrigPosTest.GetY();
+//        auto OrigPosTestY = -OrigPosTest.GetZ();
+//        auto OrigPosTestZ = -OrigPosTest.GetX();
+        Position NewOrigPos (OrigPosTestX, OrigPosTestY, OrigPosTestZ);
         testPosition = GetLocalPositionOfAbsPoint(OrigAbsPosLocalPoint,NWU)-NewOrigPos;
+        std::cout << "GetLocalPositionOfAbsPoint(OrigAbsPosLocalPoint,NWU) = " << GetLocalPositionOfAbsPoint(OrigAbsPosLocalPoint,NWU)<<std::endl;
+        std::cout << "NewOrigPos = " << NewOrigPos<<std::endl;
         EXPECT_TRUE(testPosition.isZero());
-
         
     };
 
