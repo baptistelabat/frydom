09b06d69ece65ea2dbc84b3bff3e487905b5ff2b
Lucas Letournel <lucas.letournel@dice-engineering.com>
2018-11-15 12:01:51 +0100

    [WIP] tests for the FrFrame_, all validated. Documentation added in FrFrame.

--- src/frydom/core/FrFrame.h
+++ src/frydom/core/FrFrame.h
@@ -44,92 +44,216 @@ namespace frydom {
         FrFrame_();
 
         /// Constructor taking a position and a rotation. Position is given in frame convention
+        /// \param pos Position of the frame
+        /// \param rotation Rotation of the frame
+        /// \param fc Frame convention (NED/NWU)
         FrFrame_(const Position &pos, const FrRotation_ &rotation, FRAME_CONVENTION fc);
 
-
+        /// Constructor taking a position and a quaternion. Position is given in frame convention
+        /// \param pos Position of the frame
+        /// \param quaternion Quaternion of the frame
+        /// \param fc Frame convention (NED/NWU)
         FrFrame_(const Position &pos, const FrQuaternion_& quaternion, FRAME_CONVENTION fc);
 
+        /// Copy Constructor from an other frame
+        /// \param otherFrame Frame to be copied
+        /// \return the new frame
         FrFrame_& FrFrame(const FrFrame_& otherFrame);
 
         // TODO : permettre de definir des parametres de Denavit-Hartenberg modifies...
 
 
         // Cartesian Position
-
+        /// Set the position in world reference frame of the origin of the present frame, using doubles
+        /// \param x X position of the frame
+        /// \param y Y position of the frame
+        /// \param z Z position of the frame
+        /// \param fc Frame convention (NED/NWU)
         void SetPosition(double x, double y, double z, FRAME_CONVENTION fc);
 
+        /// Set the position in world reference frame of the origin of the present frame, using a Position
+        /// \param position Position of the frame
+        /// \param fc Frame convention (NED/NWU)
         void SetPosition(const Position& position, FRAME_CONVENTION fc);
 
+        /// Get the position in world reference frame of the origin of the present frame
+        /// \param x X position of the frame
+        /// \param y Y position of the frame
+        /// \param z Z position of the frame
+        /// \param fc Frame convention (NED/NWU)
         void GetPosition(double& x, double& y, double& z, FRAME_CONVENTION fc) const;
 
+        /// Get the position in world reference frame of the origin of the present frame
+        /// \param position Position of the frame
+        /// \param fc Frame convention (NED/NWU)
         void GetPosition(Position& position, FRAME_CONVENTION fc) const;
 
+        /// Get the position in world reference frame of the origin of the present frame
+        /// \param fc Frame convention (NED/NWU)
+        /// \return  the position of the frame
         Position GetPosition(FRAME_CONVENTION fc) const;
 
+        /// Set the X position in world reference frame of the origin of the present frame
+        /// \param x X position of the frame
+        /// \param fc Frame convention (NED/NWU)
         void SetX(double x, FRAME_CONVENTION fc);
 
+        /// Set the Y position in world reference frame of the origin of the present frame
+        /// \param y Y position of the frame
+        /// \param fc Frame convention (NED/NWU)
         void SetY(double y, FRAME_CONVENTION fc);
 
+        /// Set the Z position in world reference frame of the origin of the present frame
+        /// \param z Z position of the frame
+        /// \param fc Frame convention (NED/NWU)
         void SetZ(double z, FRAME_CONVENTION fc);
 
+        /// Get the X position in world reference frame of the origin of the present frame
+        /// \param fc Frame convention (NED/NWU)
+        /// \return  the x position of the frame
         double GetX(FRAME_CONVENTION fc) const;
 
+        /// Get the Y position in world reference frame of the origin of the present frame
+        /// \param fc Frame convention (NED/NWU)
+        /// \return  the y position of the frame
         double GetY(FRAME_CONVENTION fc) const;
 
+        /// Get the Z position in world reference frame of the origin of the present frame
+        /// \param fc Frame convention (NED/NWU)
+        /// \return  the z position of the frame
         double GetZ(FRAME_CONVENTION fc) const;
 
 
         // Operations
-
+        /// The '*' operator transforms a coordinate system, so
+        /// transformations can be represented with this syntax:
+        ///  new_frame = tr_frame * old_frame;
+        /// For a sequence of transformations, i.e. a chain of coordinate
+        /// systems, you can also write this (just like you would do with
+        /// a sequence of Denavitt-Hartemberg matrix multiplications!)
+        ///  new_frame = frame1to0 * frame2to1 * frame3to2 * old_frame;
+        /// This operation is not commutative.
+        /// \param otherFrame old_frame
+        /// \return new_frame
         FrFrame_ operator*(const FrFrame_& otherFrame) const;
 
+        /// Performs post-multiplication of this frame by another
+        /// frame, for example: A*=T means  A'=A*T
+        /// \param otherFrame T
         void operator*=(const FrFrame_& otherFrame);
 
 
         // Rotation
 
+        /// Set the rotation of the present frame, using FrRotation
+        /// \param rotation Rotation to be set
         void SetRotation(const FrRotation_& rotation);
 
+        /// Set the rotation of the present frame, using FrQuaternion
+        /// \param quaternion Quaternion to be set
         void SetRotation(const FrQuaternion_& quaternion);
 
+        /// Nullify any rotation of the present frame
         void SetNoRotation();
 
+        /// Nullify any translation of the present frame transformation
         void SetNoTranslation();
 
+        /// Set the frame transformation to the Identity transformation
         void SetIdentity();
 
+        /// Get the rotation of the present frame, as FrRotation
+        /// \return the rotation of the frame/transformation frame
         FrRotation_ GetRotation() const;
 
+        /// Get the rotation of the present frame, as FrQuaternion
+        /// \return the quaternion of the frame/transformation frame
         FrQuaternion_ GetQuaternion() const;
 
+        /// Rotate the present frame, around the X axis of the present frame (if localAxis)
+        /// or around the X axis of the world reference frame otherwise, from a value given in radians.
+        /// \param angle rotation angle in radians
+        /// \param fc frame convention (NED/NWU)
+        /// \param localAxis boolean to choose between present frame x axis (true) or world frame x axis (false)
         void RotX_RADIANS(double angle, FRAME_CONVENTION fc, bool localAxis);
 
+        /// Rotate the present frame, around the X axis of the present frame (if localAxis)
+        /// or around the X axis of the world reference frame otherwise, from a value given in degrees.
+        /// \param angle rotation angle in degrees
+        /// \param fc frame convention (NED/NWU)
+        /// \param localAxis boolean to choose between present frame x axis (true) or world frame x axis (false)
         void RotX_DEGREES(double angle, FRAME_CONVENTION fc, bool localAxis);
 
+        /// Rotate the present frame, around the Y axis of the present frame (if localAxis)
+        /// or around the X axis of the world reference frame otherwise, from a value given in radians.
+        /// \param angle rotation angle in radians
+        /// \param fc frame convention (NED/NWU)
+        /// \param localAxis boolean to choose between present frame y axis (true) or world frame y axis (false)
         void RotY_RADIANS(double angle, FRAME_CONVENTION fc, bool localAxis);
 
+        /// Rotate the present frame, around the Y axis of the present frame (if localAxis)
+        /// or around the X axis of the world reference frame otherwise, from a value given in degrees.
+        /// \param angle rotation angle in degrees
+        /// \param fc frame convention (NED/NWU)
+        /// \param localAxis boolean to choose between present frame y axis (true) or world frame y axis (false)
         void RotY_DEGREES(double angle, FRAME_CONVENTION fc, bool localAxis);
 
+        /// Rotate the present frame, around the Z axis of the present frame (if localAxis)
+        /// or around the X axis of the world reference frame otherwise, from a value given in degrees.
+        /// \param angle rotation angle in radians
+        /// \param fc frame convention (NED/NWU)
+        /// \param localAxis boolean to choose between present frame z axis (true) or world frame z axis (false)
         void RotZ_RADIANS(double angle, FRAME_CONVENTION fc, bool localAxis);
 
+        /// Rotate the present frame, around the Z axis of the present frame (if localAxis)
+        /// or around the X axis of the world reference frame otherwise, from a value given in degrees.
+        /// \param angle rotation angle in degrees
+        /// \param fc frame convention (NED/NWU)
+        /// \param localAxis boolean to choose between present frame z axis (true) or world frame z axis (false)
         void RotZ_DEGREES(double angle, FRAME_CONVENTION fc, bool localAxis);
 
+        /// Set the transformation frame to be a rotation around the X axis
+        /// \param angle rotation angle in radians
+        /// \param fc frame convention (NED/NWU)
         void SetRotX_RADIANS(double angle, FRAME_CONVENTION fc);
 
+        /// Set the transformation frame to be a rotation around the X axis
+        /// \param angle rotation angle in degrees
+        /// \param fc frame convention (NED/NWU)
         void SetRotX_DEGREES(double angle, FRAME_CONVENTION fc);
 
+        /// Set the transformation frame to be a rotation around the Y axis
+        /// \param angle rotation angle in radians
+        /// \param fc frame convention (NED/NWU)
         void SetRotY_RADIANS(double angle, FRAME_CONVENTION fc);
 
+        /// Set the transformation frame to be a rotation around the Y axis
+        /// \param angle rotation angle in degrees
+        /// \param fc frame convention (NED/NWU)
         void SetRotY_DEGREES(double angle, FRAME_CONVENTION fc);
 
+        /// Set the transformation frame to be a rotation around the Z axis
+        /// \param angle rotation angle in radians
+        /// \param fc frame convention (NED/NWU)
         void SetRotZ_RADIANS(double angle, FRAME_CONVENTION fc);
 
+        /// Set the transformation frame to be a rotation around the Z axis
+        /// \param angle rotation angle in degrees
+        /// \param fc frame convention (NED/NWU)
         void SetRotZ_DEGREES(double angle, FRAME_CONVENTION fc);
 
         // TODO : et les angles d'Euler ?
 
+        /// Get the transformation frame, transforming this frame to an other frame
+        /// \param otherFrame other frame from which the transformation frame is searched
+        /// \param fc frame convention (NED/NWU)
+        /// \return the transformation frame
         FrFrame_ GetOtherFrameRelativeTransform_WRT_ThisFrame(const FrFrame_ &otherFrame, FRAME_CONVENTION fc) const;
 
+        /// Get the transformation frame, transforming an other frame to this frame
+        /// \param otherFrame other frame from which the transformation frame is searched
+        /// \param fc frame convention (NED/NWU)
+        /// \return the transformation frame
         FrFrame_ GetThisFrameRelativeTransform_WRT_OtherFrame(const FrFrame_ &otherFrame, FRAME_CONVENTION fc) const;
 
 
@@ -143,9 +267,11 @@ namespace frydom {
 
         double GetGeographicHeight(const FrOffshoreSystem_* system) const;
 
-
+        /// Inverse a frame transformation
+        /// \return the inverse frame transformation
         FrFrame_& Inverse();
-
+        /// Get the inverse of a frame transformation
+        /// \return the inverse frame transformation
         FrFrame_ GetInverse() const;
 
 
@@ -155,7 +281,7 @@ namespace frydom {
 
         std::ostream& cout(std::ostream& os) const;
 
-        friend class FrInertiaTensor_;  // TODO : voir pourquoi on definnit cette amitie... (et voir si on peut retirer !)
+        friend class FrInertiaTensor_;  // TODO : voir pourquoi on definit cette amitie... (et voir si on peut retirer !)
 
 
         // Node
@@ -189,7 +315,7 @@ namespace frydom {
 
     };
 
-    /// Transoform between frames which is also a frame.
+    /// Transform between frames which is also a frame.
     using FrTransform = FrFrame_;
 
 
