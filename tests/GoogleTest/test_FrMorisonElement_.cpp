ffafccedf257edae1abcacb2538172227e7b4a59
Camille Chauvigné <camille.chauvigne@gmail.com>
2018-12-20 10:03:16 +0100

    [Fix] in morison test

--- tests/GoogleTest/test_FrMorisonElement_.cpp
+++ tests/GoogleTest/test_FrMorisonElement_.cpp
@@ -217,6 +217,7 @@ MorisonCoeff TestMorison::ReadMorisonCoeff(FrHDF5Reader& reader, std::string fie
 void TestMorison::SetUp() {
     body = std::make_shared<FrBody_>();
     system.AddBody(body);
+    system.GetEnvironment()->GetOcean()->SetInfiniteDepth();
 }
 
 void TestMorison::LoadData(std::string filename) {
