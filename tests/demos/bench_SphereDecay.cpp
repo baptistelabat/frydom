572612779e8aa435b9d85751c34c034a3d93f07a
Camille Chauvigné <camille.chauvigne@gmail.com>
2019-02-04 14:28:49 +0100

    [Fix] close file in benchmark test

--- tests/demos/bench_SphereDecay.cpp
+++ tests/demos/bench_SphereDecay.cpp
@@ -93,7 +93,7 @@ int main(int argc, char* argv[]) {
         // ##CC
     }
 
-    //myfile.close();
+    myfile.close();
 
     std::cout << "============================== End ===================================== " << std::endl;
 
