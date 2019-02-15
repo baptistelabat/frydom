67efa844aa4c2a0f65711b5b08a1c7556ee28829
François Rongère <francois.rongere@dice-engineering.com>
2019-02-15 17:41:07 +0100

    Making MathUtils point to a commit that was actual

--- cmake/Add_MathUtils.cmake
+++ cmake/Add_MathUtils.cmake
@@ -9,7 +9,7 @@ if (NOT mathutils_FOUND)
     set(MathUtils_URL "git@d-ice.githost.io:essai_cmake/MathUtils.git") # TODO: pointer vers un depot git distant...
     FetchContent_Declare(mathutils
             GIT_REPOSITORY ${MathUtils_URL}
-            GIT_TAG master
+            GIT_TAG ab3871ab67ca274a8d8b5c88c4b56142886151c0
             )
 
     FetchContent_GetProperties(mathutils)
