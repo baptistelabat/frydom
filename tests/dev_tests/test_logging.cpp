1c088b97c7b851e9fae14189e21f47a30d455af7
Lucas Letournel <lucas.letournel@dice-engineering.com>
2019-02-25 14:01:46 +0100

    [WIP] test for logging.

---
+++ tests/dev_tests/test_logging.cpp
@@ -0,0 +1,18 @@
+//
+// Created by lletourn on 22/02/19.
+//
+
+#include "frydom/frydom.h"
+
+using namespace frydom;
+
+int main(int argc, char* argv[]) {
+
+    FrOffshoreSystem_ system;
+
+    system.Initialize();
+
+
+
+}
+
