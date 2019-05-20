51d5e0eacfdbf9fe30a87cf8266ececb26e21e24
Lucas Letournel <lucas.letournel@dice-engineering.com>
2019-05-20 12:02:54 +0200

    [WIP]

--- tests/demos/demo_Hub_Installation.cpp
+++ tests/demos/demo_Hub_Installation.cpp
@@ -96,7 +96,7 @@ int main(int argc, char* argv[]) {
 //    barge->SetPosition(Position(0.,0.,1.5),fc);
 
     auto rev1_barge_node = barge->NewNode();
-    rev1_barge_node->SetPositionInBody(Position(-7.5,0.,3.5), fc);
+    rev1_barge_node->SetPositionInBody(Position(-7.5,0.,3.), fc);
 
 //    barge->SetFixedInWorld(true);
 
@@ -136,10 +136,8 @@ int main(int argc, char* argv[]) {
 //    auto excitationForce = make_linear_excitation_force(hdb, barge);
 //
     // -- Radiation
-//    auto radiationModel = make_radiation_convolution_model(hdb, &system);
-
-
-//    radiationModel->SetImpulseResponseSize(barge.get(), 20., 0.025);
+    auto radiationModel = make_radiation_convolution_model(hdb, &system);
+    radiationModel->SetImpulseResponseSize(barge.get(), 20., 0.025);
 
     // Test
 
@@ -155,150 +153,156 @@ int main(int argc, char* argv[]) {
     base_crane->SetName("Base_Crane");
     makeItBox(base_crane, 5., 5., 2., 120e3);
     base_crane->SetColor(Red);
-    base_crane->SetPosition(Position(-7.5,0,3.5), fc);
+    base_crane->SetPosition(rev1_barge_node->GetPositionInWorld(NWU), fc);
 
     auto rev1_base_node = base_crane->NewNode();
 
-//    auto rev2_base_node = base_crane->NewNode();
-//    rev2_base_node->SetPositionInBody(Position(0.,0.,2.), fc);
-//    rev2_base_node->RotateAroundXInBody(90*DEG2RAD, NWU);
+    auto rev2_base_node = base_crane->NewNode();
+    rev2_base_node->SetPositionInBody(Position(0.,0.,2.), fc);
+    rev2_base_node->RotateAroundXInBody(90*DEG2RAD, NWU);
+
+//    auto base_crane_1 = system.NewBody();
+//    base_crane_1->SetName("Base_Crane_1");
+//    makeItBox(base_crane_1,2.5,1.5,2, 10e3);
+//    base_crane_1->SetColor(Red);
+//    base_crane_1->SetPosition(Position(-7.5,1.75,4.5), fc);
 //
-////    auto base_crane_1 = system.NewBody();
-////    base_crane_1->SetName("Base_Crane_1");
-////    makeItBox(base_crane_1,2.5,1.5,2, 10e3);
-////    base_crane_1->SetColor(Red);
-////    base_crane_1->SetPosition(Position(-7.5,1.75,4.5), fc);
-////
-////    auto base_crane_2 = system.NewBody();
-////    base_crane_2->SetName("Base_Crane_2");
-////    makeItBox(base_crane_2,2.5,1.5,2, 10e3);
-////    base_crane_2->SetColor(Red);
-////    base_crane_2->SetPosition(Position(-7.5,-1.75,4.5), fc);
-//
-//
-//    auto arm_crane = system.NewBody();
-//    arm_crane->SetName("Arm_Crane");
-//    makeItBox(arm_crane,19.,1.5,1.5,20e3);
-//    arm_crane->SetColor(DarkGreen);
-//    arm_crane->SetPositionOfBodyPoint(Position(-9.0,0.,0.), Position(-7.5,0.,6.5), fc);
-//
-//    FrRotation arm_Rotation;
-//    arm_Rotation.SetCardanAngles_DEGREES(0.,-45.,0., fc);
-//    arm_crane->RotateAroundPointInBody(arm_Rotation, Position(-9.0,0.,0.), fc);
-//
-//    auto rev2_crane_node = arm_crane->NewNode();
-//    rev2_crane_node->SetPositionInBody(Position(-9.,0.,0.), fc);
-//    rev2_crane_node->RotateAroundXInBody(90*DEG2RAD, NWU);
-//
-//    auto crane_node = arm_crane->NewNode();
-//    crane_node->SetPositionInBody(Position(9.,0.,-0.75), fc);
-//
+//    auto base_crane_2 = system.NewBody();
+//    base_crane_2->SetName("Base_Crane_2");
+//    makeItBox(base_crane_2,2.5,1.5,2, 10e3);
+//    base_crane_2->SetColor(Red);
+//    base_crane_2->SetPosition(Position(-7.5,-1.75,4.5), fc);
+
+
+    auto arm_crane = system.NewBody();
+    arm_crane->SetName("Arm_Crane");
+    makeItBox(arm_crane,19.,1.5,1.5,20e3);
+    arm_crane->SetColor(DarkGreen);
+    arm_crane->SetPositionOfBodyPoint(Position(-9.0,0.,0.), Position(-7.5,0.,6.5), fc);
+
+    FrRotation arm_Rotation;
+    arm_Rotation.SetCardanAngles_DEGREES(0.,-45.,0., fc);
+    arm_crane->RotateAroundPointInBody(arm_Rotation, Position(-9.0,0.,0.), fc);
+
+    auto rev2_crane_node = arm_crane->NewNode();
+    rev2_crane_node->SetPositionInBody(Position(-9.,0.,0.), fc);
+    rev2_crane_node->RotateAroundXInBody(90*DEG2RAD, NWU);
+
+    auto crane_node = arm_crane->NewNode();
+    crane_node->SetPositionInBody(Position(9.,0.,-0.75), fc);
+
     // --------------------------------------------------
     // links definition
     // --------------------------------------------------
 
     auto rev1 = make_revolute_link(rev1_barge_node, rev1_base_node, &system);
+    auto motor1 = rev1->Motorize(POSITION);
+//    auto function = std::make_shared<FrCosRampFunction>();
+    FrCosRampFunction function;
+    function.SetByTwoPoints(0.,0.,10.,90*DEG2RAD);
+    motor1->SetMotorFunction(function);
+
 //    rev1->SetSpringDamper(1e8, 1e8);
 //    rev1->SetRestAngle(90*DEG2RAD);
 
 
-//    auto rev2 = make_revolute_link(rev2_base_node, rev2_crane_node, &system);
-//    rev2->SetSpringDamper(5e8, 1e6);
-//    rev2->SetRestAngle(45*DEG2RAD);
-//
-//
-//    // --------------------------------------------------
-//    // hub box model
-//    // --------------------------------------------------
-//
-//    auto hub_box = system.NewBody();
-//    hub_box->SetName("Hub_Box");
-//    makeItBox(hub_box, 1.5,1.5,1.5, 20.7e3);
-//    hub_box->SetPosition(Position(crane_node->GetPositionInWorld(fc).GetX(),0.,4.25), fc);
-//
-//    auto hub_node = hub_box->NewNode();
-//    hub_node->SetPositionInBody(Position(0.,0.,0.75), fc);
-//
-//    // --------------------------------------------------
-//    // Hub Line
-//    // --------------------------------------------------
-//    // Line properties
-//    bool elastic = true;
-//    double unstretchedLength = crane_node->GetPositionInWorld(fc).GetZ() - hub_node->GetPositionInWorld(fc).GetZ();
-//    auto cableProp = make_cable_properties();
-//    cableProp->SetSectionArea(0.5);
-//    cableProp->SetEA(5e7);
-//    cableProp->SetLinearDensity(60);
-//
-//    auto CatenaryLine = make_catenary_line(crane_node, hub_node, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::AIR);
+    auto rev2 = make_revolute_link(rev2_base_node, rev2_crane_node, &system);
+    rev2->SetSpringDamper(5e8, 1e6);
+    rev2->SetRestAngle(45*DEG2RAD);
 
-//    // --------------------------------------------------
-//    // Mooring Lines
-//    // --------------------------------------------------
-//    double buoyRadius = 1.5;
-//    double buoyMass = 100;
-//    double buoyDamping = 1000;
-//
-//
-//    auto buoySE = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
-//    buoySE->SetName("SE");
-//    buoySE->SetPosition(Position(-50.,-25.,0.), NWU);
-//    auto buoyNodeSE = buoySE->NewNode();
-//
-//    auto buoySW = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
-//    buoySW->SetName("SW");
-//    buoySW->SetPosition(Position(-50.,25.,0.), NWU);
-//    auto buoyNodeSW = buoySW->NewNode();
-//
-//    auto buoyNE = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
-//    buoyNE->SetName("NE");
-//    buoyNE->SetPosition(Position(50.,-25.,0.), NWU);
-//    auto buoyNodeNE = buoyNE->NewNode();
-//
-//    auto buoyNW = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
-//    buoyNW->SetName("NW");
-//    buoyNW->SetPosition(Position(50.,25.,0.), NWU);
-//    auto buoyNodeNW = buoyNW->NewNode();
-//
-//
-//    auto worldNodeSE = system.GetWorldBody()->NewNode();
-//    worldNodeSE->SetPositionInBody(Position(-125.,-75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
-//    auto worldNodeNE = system.GetWorldBody()->NewNode();
-//    worldNodeNE->SetPositionInBody(Position(125.,-75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
-//    auto worldNodeSW = system.GetWorldBody()->NewNode();
-//    worldNodeSW->SetPositionInBody(Position(-125.,75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
-//    auto worldNodeNW = system.GetWorldBody()->NewNode();
-//    worldNodeNW->SetPositionInBody(Position(125.,75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
-//
-//    auto bargeNodeSE = barge->NewNode();
-//    bargeNodeSE->SetPositionInBody(Position(-12.5,-5.,0.),fc);
-//    auto bargeNodeSW = barge->NewNode();
-//    bargeNodeSW->SetPositionInBody(Position(-12.5,5.,0.),fc);
-//    auto bargeNodeNE = barge->NewNode();
-//    bargeNodeNE->SetPositionInBody(Position(12.5,-5.,0.),fc);
-//    auto bargeNodeNW = barge->NewNode();
-//    bargeNodeNW->SetPositionInBody(Position(12.5,5.,0.),fc);
-//
-//    /// Mooring lines length
-//
-//    unstretchedLength = 95;
-////    linearDensity = 20.;
-////    EA = 5e7;
-////    sectionArea = 0.025;
-////    YoungModulus = EA / sectionArea;
-//    double Lv = 42.5;
-//
-//    auto mooringLineSE = make_catenary_line(worldNodeSE, buoyNodeSE, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
-//    auto tetherLineSE = make_catenary_line(bargeNodeSE, buoyNodeSE, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
-//
-//    auto mooringLineSW = make_catenary_line(worldNodeSW, buoyNodeSW, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
-//    auto tetherLineSW = make_catenary_line(bargeNodeSW, buoyNodeSW, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
-//
-//    auto mooringLineNE = make_catenary_line(worldNodeNE, buoyNodeNE, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
-//    auto tetherLineNE = make_catenary_line(bargeNodeNE, buoyNodeNE, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
-//
-//    auto mooringLineNW = make_catenary_line(worldNodeNW, buoyNodeNW, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
-//    auto tetherLineNW = make_catenary_line(bargeNodeNW, buoyNodeNW, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
+
+    // --------------------------------------------------
+    // hub box model
+    // --------------------------------------------------
+
+    auto hub_box = system.NewBody();
+    hub_box->SetName("Hub_Box");
+    makeItBox(hub_box, 1.5,1.5,1.5, 20.7e3);
+    hub_box->SetPosition(Position(crane_node->GetPositionInWorld(fc).GetX(),0.,4.25), fc);
+
+    auto hub_node = hub_box->NewNode();
+    hub_node->SetPositionInBody(Position(0.,0.,0.75), fc);
+
+    // --------------------------------------------------
+    // Hub Line
+    // --------------------------------------------------
+    // Line properties
+    bool elastic = true;
+    double unstretchedLength = crane_node->GetPositionInWorld(fc).GetZ() - hub_node->GetPositionInWorld(fc).GetZ();
+    auto cableProp = make_cable_properties();
+    cableProp->SetSectionArea(0.5);
+    cableProp->SetEA(5e7);
+    cableProp->SetLinearDensity(600);
+
+    auto CatenaryLine = make_catenary_line(crane_node, hub_node, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::AIR);
+
+    // --------------------------------------------------
+    // Mooring Lines
+    // --------------------------------------------------
+    double buoyRadius = 1.5;
+    double buoyMass = 100;
+    double buoyDamping = 1000;
+
+
+    auto buoySE = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
+    buoySE->SetName("SE");
+    buoySE->SetPosition(Position(-50.,-25.,0.), NWU);
+    auto buoyNodeSE = buoySE->NewNode();
+
+    auto buoySW = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
+    buoySW->SetName("SW");
+    buoySW->SetPosition(Position(-50.,25.,0.), NWU);
+    auto buoyNodeSW = buoySW->NewNode();
+
+    auto buoyNE = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
+    buoyNE->SetName("NE");
+    buoyNE->SetPosition(Position(50.,-25.,0.), NWU);
+    auto buoyNodeNE = buoyNE->NewNode();
+
+    auto buoyNW = make_mooring_buoy(&system, buoyRadius, buoyMass, true, buoyDamping);
+    buoyNW->SetName("NW");
+    buoyNW->SetPosition(Position(50.,25.,0.), NWU);
+    auto buoyNodeNW = buoyNW->NewNode();
+
+
+    auto worldNodeSE = system.GetWorldBody()->NewNode();
+    worldNodeSE->SetPositionInBody(Position(-125.,-75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
+    auto worldNodeNE = system.GetWorldBody()->NewNode();
+    worldNodeNE->SetPositionInBody(Position(125.,-75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
+    auto worldNodeSW = system.GetWorldBody()->NewNode();
+    worldNodeSW->SetPositionInBody(Position(-125.,75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
+    auto worldNodeNW = system.GetWorldBody()->NewNode();
+    worldNodeNW->SetPositionInBody(Position(125.,75.,-system.GetEnvironment()->GetOcean()->GetDepth(fc)), fc);
+
+    auto bargeNodeSE = barge->NewNode();
+    bargeNodeSE->SetPositionInBody(Position(-12.5,-5.,0.),fc);
+    auto bargeNodeSW = barge->NewNode();
+    bargeNodeSW->SetPositionInBody(Position(-12.5,5.,0.),fc);
+    auto bargeNodeNE = barge->NewNode();
+    bargeNodeNE->SetPositionInBody(Position(12.5,-5.,0.),fc);
+    auto bargeNodeNW = barge->NewNode();
+    bargeNodeNW->SetPositionInBody(Position(12.5,5.,0.),fc);
+
+    /// Mooring lines length
+
+    unstretchedLength = 95;
+//    linearDensity = 20.;
+//    EA = 5e7;
+//    sectionArea = 0.025;
+//    YoungModulus = EA / sectionArea;
+    double Lv = 42.5;
+
+    auto mooringLineSE = make_catenary_line(worldNodeSE, buoyNodeSE, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
+    auto tetherLineSE = make_catenary_line(bargeNodeSE, buoyNodeSE, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
+
+    auto mooringLineSW = make_catenary_line(worldNodeSW, buoyNodeSW, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
+    auto tetherLineSW = make_catenary_line(bargeNodeSW, buoyNodeSW, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
+
+    auto mooringLineNE = make_catenary_line(worldNodeNE, buoyNodeNE, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
+    auto tetherLineNE = make_catenary_line(bargeNodeNE, buoyNodeNE, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
+
+    auto mooringLineNW = make_catenary_line(worldNodeNW, buoyNodeNW, &system, cableProp, elastic, unstretchedLength, FLUID_TYPE::WATER);
+    auto tetherLineNW = make_catenary_line(bargeNodeNW, buoyNodeNW, &system, cableProp, elastic, Lv, FLUID_TYPE::WATER);
 
     // ------------------ Run ------------------ //
 
