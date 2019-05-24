Bug in the computation of the Froude-Krylov loads.

tools/frydom/pyHDB/body_db_v2.py
@@ -100,13 +100,17 @@ class BodyDB(object):
         centers = self.mesh.faces_centers
 
         for i in range(0,6):
+
+            # Direction.
             direction = np.zeros(3)
-            if (i == 0 or i == 4):
+            if (i == 0 or i == 3):
                 direction[0] = 1  # ex.
-            elif (i == 1 or i == 5):
+            elif (i == 1 or i == 4):
                 direction[1] = 1  # ey.
-            elif (i == 2 or i == 6):
+            elif (i == 2 or i == 5):
                 direction[2] = 1  # ez.
+
+            # n*ds.
             if i <= 2: # Force.
                 self._nds[i, :] = areas * np.einsum('ij, j -> i', normals, direction)
             else:  # Moment.
@@ -127,6 +131,7 @@ class BodyDB(object):
         np.ndarray
             (n_faces) Array of ndS quantities.
         """
+
         if self._nds is None:
             self._compute_nds()
 