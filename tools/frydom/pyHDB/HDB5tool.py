61752f28f83c2f7491117eb96a6f163e93ea267c
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2019-06-04 11:31:37 +0200

    ArgParse of the plots.

--- tools/frydom/pyHDB/HDB5tool.py
+++ tools/frydom/pyHDB/HDB5tool.py
@@ -33,16 +33,22 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--edition','-ed', action="store",help="""
             Edition of FRyDoM: CE or ce for FRyDoM-CE, EE or ee for FRyDoM-EE.""")
 
+parser.add_argument('-ee','-EE', action="store_true",help="""
+            Use of FRyDoM EE.""")
+
+parser.add_argument('-ce','-CE', action="store_true",help="""
+            Use of FRyDoM CE.""")
+
 # Path to Nemoh.cal.
 parser.add_argument('--path_to_nemoh_cal','-cal', action="store",help="""
             Path to the folder including the file Nemoh.cal.""")
 
 # Discretization - Wave directions.
-parser.add_argument('--discretization_waves','--discretization_wave','-dis_waves', '-dis_wave', action="store",help="""
+parser.add_argument('--discretization_waves','--discretization_wave','-dis_waves', '-dis_wave', '-dw', action="store",help="""
             Integer for the new discretization of the wave directions.""")
 
 # Discretization - Wave frequencies.
-parser.add_argument('--discretization_frequencies','--discretization_freq','--discretization_frequency','-dis_freq','-dis_frequency','-dis_frequencies', action="store",help="""
+parser.add_argument('--discretization_frequencies','--discretization_freq','--discretization_frequency','-dis_freq','-dis_frequency','-dis_frequencies','-df', action="store",help="""
             Integer for the new discretization of the wave frequencies.""")
 
 # Discretization - Wave frequencies.
@@ -62,8 +68,12 @@ parser.add_argument('--activate_inertia', '-active_i', action="append",help="""
             Activate inertia for the body of index given in argument.""")
 
 # Body - Inertia matrix.
-parser.add_argument('--inertia', '-i', nargs=7, metavar=('id', 'i33', 'i44', 'i55', 'i34', 'i35', 'i45'), action="append",help="""
-            Inertia coefficients (K33, K44, K55, K34, K35, K45) for the body of index given in first argument.""")
+parser.add_argument('--inertia', '-i', nargs=8, metavar=('id', 'mass', 'i44', 'i55', 'i66', 'i45', 'i46', 'i56'), action="append",help="""
+            Inertia coefficients (Mass, I44, I55, I66, I45, I46, I56) for the body of index given in first argument.""")
+
+# Body - Inertia matrix.
+parser.add_argument('--inertia_only', '-io', nargs=7, metavar=('id', 'i44', 'i55', 'i66', 'i45', 'i46', 'i56'), action="append",help="""
+            Inertia coefficients (I44, I55, I66, I45, I46, I56) for the body of index given in first argument.""")
 
 # Body - Mass.
 parser.add_argument('--mass', '--Mass', '-m', nargs=2, metavar=('id', 'mass'), action="append",help="""
@@ -85,6 +95,30 @@ parser.add_argument('--no_sym_hdb','-ns', action="store_true",help="""
 parser.add_argument('--write', '--export','-w', action="store",help="""
             Writing the hdb5 output file with the given name.""")
 
+# Plot added mass and damping coefficients.
+parser.add_argument('--plot_radiation', '--plots_radiation', '--plot_AB','-pab', '-prad', nargs = 4, metavar=('ibody_force, iforce, ibody_motion, idof'), action="append",help="""
+            Plot the added mass and damping coefficients of ibody along iforce for a motion of ibody_motion along idof.""")
+
+# Plot diffraction loads.
+parser.add_argument('--plot_diffraction', '--plots_diffraction', '--plot_diff','-pd', '-pdiff', nargs = 3, metavar=('ibody, iforce, iwave'), action="append",help="""
+            Plot the diffraction loads of ibody along iforce for iwave.""")
+
+# Plot Froude-Krylov loads.
+parser.add_argument('--plot_froude_krylov', '--plots_froude_krylov', '--plot_fk','-pfk', nargs = 3, metavar=('ibody, iforce, iwave'), action="append",help="""
+            Plot the Froude-Krylov loads of ibody along iforce for iwave.""")
+
+# Plot excitation loads.
+parser.add_argument('--plot_excitation', '--plots_excitation', '--plot_exc','-pe', '-pexc', nargs = 3, metavar=('ibody, iforce, iwave'), action="append",help="""
+            Plot the excitation loads of ibody along iforce for iwave.""")
+
+# Plot IRF.
+parser.add_argument('--plot_irf', '--plots_irf','-pirf', nargs = 4, metavar=('ibody_force, iforce, ibody_motion, idof'), action="append",help="""
+            Plot the impulse response functions of ibody along iforce for a motion of ibody_motion along idof.""")
+
+# Plot IRF speed.
+parser.add_argument('--plot_irf_speed', '--plots_irf_speed', '--plot_irf_ku','-pirfs','-pirfku', '-pirf_speed', nargs = 4, metavar=('ibody_force, iforce, ibody_motion, idof'), action="append",help="""
+            Plot the impulse response functions with speed velocity of ibody along iforce for a motion of ibody_motion along idof.""")
+
 def main():
 
     ####################################################################################################################
@@ -97,7 +131,7 @@ def main():
     args, unknown = parser.parse_known_args()
 
     # Edition.
-    if (args.edition != None):
+    if (args.edition is not None and args.ce is False and args.ee is False):
         Edition = args.edition
         if(Edition == 'CE' or Edition == 'GPL' or Edition == 'Community' or Edition == 'community' or Edition == 'ce' or Edition == 'gpl'):
             Edition = 'CE'
@@ -105,6 +139,10 @@ def main():
             Edition = 'EE'
         else:
             print("The edition given in input is not known, please choose between CE or EE.")
+    elif(args.edition is None and args.ce is True and args.ee is False):
+        Edition = 'CE'
+    elif(args.edition is None and args.ce is False and args.ee is True):
+        Edition = 'EE'
     else:
         print("The edition of FRyDoM is required. Please use --edition or -ed.")
         exit()
@@ -169,19 +207,32 @@ def main():
         nb_inertia = len(args.inertia)
         for j in range(0, nb_inertia):
             database.body[int(args.inertia[j][0]) - 1].activate_inertia()
-            database.body[int(args.inertia[j][0]) - 1].inertia.I44 = args.inertia[j][1]
-            database.body[int(args.inertia[j][0]) - 1].inertia.I55 = args.inertia[j][2]
-            database.body[int(args.inertia[j][0]) - 1].inertia.I66 = args.inertia[j][3]
-            database.body[int(args.inertia[j][0]) - 1].inertia.I45 = args.inertia[j][4]
-            database.body[int(args.inertia[j][0]) - 1].inertia.I46 = args.inertia[j][5]
-            database.body[int(args.inertia[j][0]) - 1].inertia.I56 = args.inertia[j][6]
+            database.body[int(args.inertia[j][0]) - 1].inertia.mass = args.inertia[j][1]
+            database.body[int(args.inertia[j][0]) - 1].inertia.I44 = args.inertia[j][2]
+            database.body[int(args.inertia[j][0]) - 1].inertia.I55 = args.inertia[j][3]
+            database.body[int(args.inertia[j][0]) - 1].inertia.I66 = args.inertia[j][4]
+            database.body[int(args.inertia[j][0]) - 1].inertia.I45 = args.inertia[j][5]
+            database.body[int(args.inertia[j][0]) - 1].inertia.I46 = args.inertia[j][6]
+            database.body[int(args.inertia[j][0]) - 1].inertia.I56 = args.inertia[j][7]
+
+    # Body - Inertia matrix.
+    if (args.inertia_only is not None):
+        nb_inertia = len(args.inertia_only)
+        for j in range(0, inertia_only):
+            database.body[int(args.inertia_only[j][0]) - 1].activate_inertia()
+            database.body[int(args.inertia_only[j][0]) - 1].inertia.I44 = args.inertia_only[j][1]
+            database.body[int(args.inertia_only[j][0]) - 1].inertia.I55 = args.inertia_only[j][2]
+            database.body[int(args.inertia_only[j][0]) - 1].inertia.I66 = args.inertia_only[j][3]
+            database.body[int(args.inertia_only[j][0]) - 1].inertia.I45 = args.inertia_only[j][4]
+            database.body[int(args.inertia_only[j][0]) - 1].inertia.I46 = args.inertia_only[j][5]
+            database.body[int(args.inertia_only[j][0]) - 1].inertia.I56 = args.inertia_only[j][6]
 
     # Body - Mass.
     if (args.mass is not None):
         nb_mass = len(args.mass)
         for j in range(0, nb_mass):
-            database.body[int(args.inertia[j][0]) - 1].activate_inertia()
-            database.body[int(args.inertia[j][0]) - 1].inertia.mass = args.mass[j][1]
+            database.body[int(args.mass[j][0]) - 1].activate_inertia()
+            database.body[int(args.mass[j][0]) - 1].inertia.mass = args.mass[j][1]
 
     # Response Amplitude Operators.
     if(args.RAO is not None and Edition == 'EE'):
@@ -194,12 +245,50 @@ def main():
         Drift.computeDriftForce()
 
     # Symmetry of the HDB.
-    if(args.no_sym_hdb is None):
+    if(args.no_sym_hdb is False):
         database.symmetry_HDB()
 
     # Writing the hdb5 output file.
     if(args.write is not None):
         database.export_hdb5(args.write)
 
+    # Plot added mass and damping coefficients.
+    if (args.plot_radiation is not None):
+        nb_plots_radiation = len(args.plot_radiation)
+        for j in range(0, nb_plots_radiation):
+            database.Plot_Radiation_coeff(ibody_force=int(args.plot_radiation[j][0]), iforce=int(args.plot_radiation[j][1]), ibody_motion=int(args.plot_radiation[j][2]),
+                                          idof=int(args.plot_radiation[j][3]))
+
+    # Plot diffraction loads.
+    if (args.plot_diffraction is not None):
+        nb_plots_diffraction = len(args.plot_diffraction)
+        for j in range(0, nb_plots_diffraction):
+            database.Plot_Diffraction(ibody=int(args.plot_diffraction[j][0]), iforce=int(args.plot_diffraction[j][1]), iwave=int(args.plot_diffraction[j][2]))
+
+    # Plot Froude-Krylov loads.
+    if (args.plot_froude_krylov is not None):
+        nb_plots_froude_krylov = len(args.plot_froude_krylov)
+        for j in range(0, nb_plots_froude_krylov):
+            database.Plot_Froude_Krylov(ibody=int(args.plot_froude_krylov[j][0]), iforce=int(args.plot_froude_krylov[j][1]), iwave=int(args.plot_froude_krylov[j][2]))
+
+    # Plot excitation loads.
+    if (args.plot_excitation is not None):
+        nb_plots_excitation = len(args.plot_excitation)
+        for j in range(0, nb_plots_excitation):
+            database.Plot_Excitation(ibody=int(args.plot_excitation[j][0]), iforce=int(args.plot_excitation[j][1]), iwave=int(args.plot_excitation[j][2]))
+
+    # Plot impulse response functions.
+    if (args.plot_irf is not None):
+        nb_plots_irf = len(args.plot_irf)
+        for j in range(0, nb_plots_irf):
+            database.Plot_IRF(ibody_force=int(args.plot_irf[j][0]), iforce=int(args.plot_irf[j][1]), ibody_motion=int(args.plot_irf[j][2]),
+                                          idof=int(args.plot_irf[j][3]))
+
+    # Plot impulse response function with speed velocity.
+    if (args.plot_irf_speed is not None):
+        nb_plots_irf_speed = len(args.plot_irf_speed)
+        for j in range(0, nb_plots_irf_speed):
+            database.Plot_IRF_speed(ibody_force=int(args.plot_irf_speed[j][0]), iforce=int(args.plot_irf_speed[j][1]), ibody_motion=int(args.plot_irf_speed[j][2]),
+                              idof=int(args.plot_irf_speed[j][3]))
 if __name__ == '__main__':
     main()
