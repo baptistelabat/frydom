7faa8b81c6829a9f2c01cebf2f2e798debbface9
Lucas Letournel <lucas.letournel@dice-engineering.com>
2019-06-19 16:29:15 +0200

    [FIX] unit for acceleration in FrBody logs

--- src/frydom/core/body/FrBody.cpp
+++ src/frydom/core/body/FrBody.cpp
@@ -1012,15 +1012,15 @@ namespace frydom {
 
         // Body Acceleration
         m_message->AddField<Eigen::Matrix<double, 3, 1>>
-                ("LinearAccelerationInWorld","m/s²", fmt::format("body linear acceleration in the world reference frame in {}", GetLogFrameConvention()),
+                ("LinearAccelerationInWorld","m/s^2", fmt::format("body linear acceleration in the world reference frame in {}", GetLogFrameConvention()),
                  [this]() {return GetAccelerationInWorld(GetLogFrameConvention());});
         // Body COG Acceleration
         m_message->AddField<Eigen::Matrix<double, 3, 1>>
-                ("LinearCOGAccelerationInWorld","m/s²", fmt::format("COG body linear acceleration in the world reference frame in {}", GetLogFrameConvention()),
+                ("LinearCOGAccelerationInWorld","m/s^2", fmt::format("COG body linear acceleration in the world reference frame in {}", GetLogFrameConvention()),
                  [this]() {return GetCOGAccelerationInWorld(GetLogFrameConvention());});
         // Body Angular Acceleration
         m_message->AddField<Eigen::Matrix<double, 3, 1>>
-                ("AngularAccelerationInWorld","rad/s²", fmt::format("body angular acceleration in the world reference frame in {}", GetLogFrameConvention()),
+                ("AngularAccelerationInWorld","rad/s^2", fmt::format("body angular acceleration in the world reference frame in {}", GetLogFrameConvention()),
                  [this]() {return GetAngularAccelerationInWorld(GetLogFrameConvention());});
 
 
