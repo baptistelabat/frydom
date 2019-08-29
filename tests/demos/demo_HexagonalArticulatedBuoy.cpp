a80c9e3484523bcdfbe9f39705916554c5c5fe31
Lucas Letournel <lucas.letournel@dice-engineering.com>
2019-08-29 10:23:52 +0200

    [ADD] mooring lines and current forces through quad damping forces

--- tests/demos/demo_HexagonalArticulatedBuoy.cpp
+++ tests/demos/demo_HexagonalArticulatedBuoy.cpp
@@ -6,6 +6,31 @@
 
 using namespace frydom;
 
+void SetUpEnvironment(FrOffshoreSystem* system) {
+    // Environment
+
+    auto ocean = system->GetEnvironment()->GetOcean();
+    auto waveField = ocean->GetFreeSurface()->SetAiryRegularWaveField();
+    waveField->SetWavePeriod(3.);
+    waveField->SetWaveHeight(0.1); //0.1
+    waveField->SetDirection(0., DEG, NWU, GOTO);
+
+    system->GetEnvironment()->GetTimeRamp()->SetByTwoPoints(0., 0., 15., 1.);
+    system->GetEnvironment()->GetTimeRamp()->SetActive(false);
+
+    ocean->GetFreeSurface()->GetFreeSurfaceGridAsset()->SetGrid(-5., 10., 0.5, -5., 5., 0.5);
+    ocean->GetFreeSurface()->GetFreeSurfaceGridAsset()->SetUpdateStep(5);
+
+    ocean->GetSeabed()->SetBathymetry(-7.5, NWU);
+    ocean->GetSeabed()->GetSeabedGridAsset()->SetGrid(-10., 15., 0.5, -12., 12., 0.5);
+//    ocean->ShowSeabed(false);
+
+    auto current = ocean->GetCurrent()->GetFieldUniform();
+    current->SetNorth(0.5, KNOT, GOTO);
+}
+
+
+
 int main(int argc, char* argv[]) {
 
     // System
@@ -18,26 +43,14 @@ int main(int argc, char* argv[]) {
     system.SetName("HexagonalArticulatedBuoy");
 
     // Environment
-
-    auto ocean = system.GetEnvironment()->GetOcean();
-    auto waveField = ocean->GetFreeSurface()->SetAiryRegularWaveField();
-    waveField->SetWavePeriod(3.);
-    waveField->SetWaveHeight(0.1); //0.1
-    waveField->SetDirection(0., DEG, NWU, GOTO);
-
-    system.GetEnvironment()->GetTimeRamp()->SetByTwoPoints(0., 0., 15., 1.);
-    system.GetEnvironment()->GetTimeRamp()->SetActive(false);
-
-    ocean->GetFreeSurface()->GetFreeSurfaceGridAsset()->SetGrid(-5., 5., 0.5, -5., 5., 0.5);
-    ocean->GetFreeSurface()->GetFreeSurfaceGridAsset()->SetUpdateStep(5);
-    ocean->ShowSeabed(false);
+    SetUpEnvironment(&system);
 
     // Bodies
 
     auto cyl1 = system.NewBody();
     cyl1->SetName("cyl1");
     cyl1->AddMeshAsset(system.GetDataPath("cylinder_base_bar.obj"));
-    cyl1->SetColor(Green);
+    cyl1->SetColor(Yellow);
     cyl1->SetPosition(Position(-2.498, 0., 0.), NWU);
     FrInertiaTensor inertiaTensor1(
             805.033,
@@ -156,17 +169,17 @@ int main(int argc, char* argv[]) {
     node_6b->SetPositionInWorld(Position(-2.498, -1.444, 0.), NWU);
     auto link6 = make_spherical_link(node_6a, node_6b, &system);
 
-    auto node_7a = cyl1->NewNode();
-    node_7a->SetPositionInWorld(Position(-2.498, 0., 0.), NWU);
-    auto node_7b = system.GetWorldBody()->NewNode();
-    node_7b->SetPositionInWorld(Position(-2.498, 0., 0.), NWU);
-    auto link7 = make_prismatic_link(node_7a, node_7b, &system);
-
-    auto node_8a = cyl4->NewNode();
-    node_8a->SetPositionInWorld(Position(2.498, 0., 0.), NWU);
-    auto node_8b = system.GetWorldBody()->NewNode();
-    node_8b->SetPositionInWorld(Position(2.498, 0., 0.), NWU);
-    auto link8 = make_prismatic_link(node_8a, node_8b, &system);
+//    auto node_7a = cyl1->NewNode();
+//    node_7a->SetPositionInWorld(Position(-2.498, 0., 0.), NWU);
+//    auto node_7b = system.GetWorldBody()->NewNode();
+//    node_7b->SetPositionInWorld(Position(-2.498, 0., 0.), NWU);
+//    auto link7 = make_prismatic_link(node_7a, node_7b, &system);
+//
+//    auto node_8a = cyl4->NewNode();
+//    node_8a->SetPositionInWorld(Position(2.498, 0., 0.), NWU);
+//    auto node_8b = system.GetWorldBody()->NewNode();
+//    node_8b->SetPositionInWorld(Position(2.498, 0., 0.), NWU);
+//    auto link8 = make_prismatic_link(node_8a, node_8b, &system);
 
     // Hydrodynamics
 
@@ -254,6 +267,116 @@ int main(int argc, char* argv[]) {
     auto excitationForce4 = make_linear_excitation_force(hdb, cyl4);
     auto excitationForce5 = make_linear_excitation_force(hdb, cyl5);
     auto excitationForce6 = make_linear_excitation_force(hdb, cyl6);
+
+    // Morison
+
+    auto cd = 3.2;
+
+    auto morisonModel1 = make_morison_model(cyl1.get());
+    //morisonModel1->AddElement(FrFrame(), 2., 1., 0., cd, 0. );
+    morisonModel1->AddElement(Position(0., -1, 0.), Position(0., 1., 0.), 1., 0., cd, 0., 5);
+    auto morisonForce1 = make_morison_force(morisonModel1, cyl1);
+
+    auto morisonModel2 = make_morison_model(cyl2.get());
+    //morisonModel2->AddElement(FrFrame(), 2., 1., 0., cd, 0. );
+    morisonModel2->AddElement(Position(-0.866, -0.5, 0.), Position(0.866, 0.5, 0.), 1., 0., cd, 0., 5);
+    auto morisonForce2 = make_morison_force(morisonModel2, cyl2);
+
+    auto morisonModel3 = make_morison_model(cyl3.get());
+    //morisonModel3->AddElement(FrFrame(), 2., 1., 0., cd, 0. );
+    morisonModel3->AddElement(Position(-0.866, +0.5, 0.), Position(0.866, -0.5, 0.), 1., 0., cd, 0., 5);
+    auto morisonForce3 = make_morison_force(morisonModel3, cyl3);
+
+    auto morisonModel4 = make_morison_model(cyl4.get());
+    //morisonModel4->AddElement(FrFrame(), 2., 1., 0., cd, 0. );
+    morisonModel4->AddElement(Position(0., -1, 0.), Position(0., 1., 0.), 1., 0., cd, 0., 5);
+    auto morisonForce4 = make_morison_force(morisonModel4, cyl4);
+
+    auto morisonModel5 = make_morison_model(cyl5.get());
+    //morisonModel5->AddElement(FrFrame(), 2., 1., 0., cd, 0. );
+    morisonModel5->AddElement(Position(-0.866, -0.5, 0.), Position(0.866, 0.5, 0.), 1., 0., cd, 0., 5);
+    auto morisonForce5 = make_morison_force(morisonModel5, cyl5);
+
+    auto morisonModel6 = make_morison_model(cyl6.get());
+    //morisonModel6->AddElement(FrFrame(), 2., 1., 0., cd, 0. );
+    morisonModel6->AddElement(Position(-0.866, +0.5, 0.), Position(0.866, -0.5, 0.), 1., 0., cd, 0., 5);
+    auto morisonForce6 = make_morison_force(morisonModel6, cyl6);
+
+
+    // Mooring Line
+
+    bool elastic = false;
+    double uLength = 12.5;
+
+    auto cableProp = make_cable_properties();
+    cableProp->SetSectionArea(0.00196);
+    cableProp->SetEA(2.95E5);
+    cableProp->SetLinearDensity(9.03);
+
+    auto worldNodeNW = system.GetWorldBody()->NewNode();
+    worldNodeNW->SetPositionInWorld(Position(-10, 5.76, -7.5), NWU);
+    auto bodyNodeNW = cyl1->NewNode();
+    bodyNodeNW->SetPositionInWorld(Position(-2.498, 1.444, 0.), NWU);
+
+    auto worldNodeN = system.GetWorldBody()->NewNode();
+    worldNodeN->SetPositionInWorld(Position(0., 11.548, -7.5), NWU);
+    auto bodyNodeN = cyl2->NewNode();
+    bodyNodeN->SetPositionInWorld(Position(0., 2.887, 0.), NWU);
+
+    auto worldNodeNE = system.GetWorldBody()->NewNode();
+    worldNodeNE->SetPositionInWorld(Position(10., 5.76, -7.5), NWU);
+    auto bodyNodeNE = cyl3->NewNode();
+    bodyNodeNE->SetPositionInWorld(Position(2.498, 1.444, 0.), NWU);
+
+    auto worldNodeSE = system.GetWorldBody()->NewNode();
+    worldNodeSE->SetPositionInWorld(Position(10., -5.76, -7.5), NWU);
+    auto bodyNodeSE = cyl4->NewNode();
+    bodyNodeSE->SetPositionInWorld(Position(2.498, -1.444, 0.), NWU);
+
+    auto worldNodeS = system.GetWorldBody()->NewNode();
+    worldNodeS->SetPositionInWorld(Position(0., -11.548, -7.5), NWU);
+    auto bodyNodeS = cyl5->NewNode();
+    bodyNodeS->SetPositionInWorld(Position(0., -2.887, 0.), NWU);
+
+    auto worldNodeSW = system.GetWorldBody()->NewNode();
+    worldNodeSW->SetPositionInWorld(Position(-10., -5.76, -7.5), NWU);
+    auto bodyNodeSW = cyl6->NewNode();
+    bodyNodeSW->SetPositionInWorld(Position(-2.498, -1.444, 0.), NWU);
+
+    auto mooringLineNW = make_catenary_line(worldNodeNW, bodyNodeNW, &system, cableProp, elastic, uLength, FLUID_TYPE::WATER);
+    auto mooringLineN  = make_catenary_line(worldNodeN, bodyNodeN, &system, cableProp, elastic, uLength, FLUID_TYPE::WATER);
+    auto mooringLineNE = make_catenary_line(worldNodeNE, bodyNodeNE, &system, cableProp, elastic, uLength, FLUID_TYPE::WATER);
+    auto mooringLineSE = make_catenary_line(worldNodeSE, bodyNodeSE, &system, cableProp, elastic, uLength, FLUID_TYPE::WATER);
+    auto mooringLineS  = make_catenary_line(worldNodeS, bodyNodeS, &system, cableProp, elastic, uLength, FLUID_TYPE::WATER);
+    auto mooringLineEW = make_catenary_line(worldNodeSW, bodyNodeSW, &system, cableProp, elastic, uLength, FLUID_TYPE::WATER);
+
+    // Current force as quadratic damping
+
+    double Su, Sv, Sw; Su = 2.; Sv = MU_PI ; Sw = 2.;
+
+    auto currentForce1 = make_quadratic_damping_force(cyl1, WATER, true);
+    currentForce1->SetProjectedSections(Su,Sv,Sw);
+    currentForce1->SetDampingCoefficients(1.,1.,1.);
+
+    auto currentForce2 = make_quadratic_damping_force(cyl2, WATER, true);
+    currentForce2->SetProjectedSections(1.5,1.5,Sw);
+    currentForce2->SetDampingCoefficients(1.,1.,1.);
+
+    auto currentForce3 = make_quadratic_damping_force(cyl3, WATER, true);
+    currentForce3->SetProjectedSections(1.5,1.5,Sw);
+    currentForce3->SetDampingCoefficients(1.,1.,1.);
+
+    auto currentForce4 = make_quadratic_damping_force(cyl4, WATER, true);
+    currentForce4->SetProjectedSections(Su,Sv,Sw);
+    currentForce4->SetDampingCoefficients(1.,1.,1.);
+
+    auto currentForce5 = make_quadratic_damping_force(cyl5, WATER, true);
+    currentForce5->SetProjectedSections(1.5,1.5,Sw);
+    currentForce5->SetDampingCoefficients(1.,1.,1.);
+
+    auto currentForce6 = make_quadratic_damping_force(cyl6, WATER, true);
+    currentForce6->SetProjectedSections(1.5,1.5,Sw);
+    currentForce6->SetDampingCoefficients(1.,1.,1.);
 
     // Simulation
 
