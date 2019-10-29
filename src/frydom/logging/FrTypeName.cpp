59c8ce2b576d0f5ee5e4aff52eabbbcca799b364
François Rongère <francois.rongere@dice-engineering.com>
2019-10-29 15:32:09 +0100

    Adding FrTypeName.cpp

---
+++ src/frydom/logging/FrTypeName.cpp
@@ -0,0 +1,18 @@
+//
+// Created by frongere on 29/10/19.
+//
+
+
+#include "FrTypeName.h"
+
+
+namespace frydom {
+
+  class FrOffshoreSystem;
+  TYPE_TO_STRING(FrOffshoreSystem, "OffshoreSystem")
+
+
+
+
+}
+
