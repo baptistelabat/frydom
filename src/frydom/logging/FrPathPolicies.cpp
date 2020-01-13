a1deeba22191f5ddb7af2c7898dcfc4ef9714e3c
Lucas Letournel <lucas.letournel@dice-engineering.com>
2020-01-13 17:25:14 +0100

    [CLEAN] after merge

--- src/frydom/logging/FrPathPolicies.cpp
+++ src/frydom/logging/FrPathPolicies.cpp
@@ -19,10 +19,6 @@ namespace frydom {
   class FrNode;
   TYPE_TO_NORMALIZED_PATH_PREFIX(FrNode, "NODES/NODE_");
 
-//<<<<<<< HEAD
-//  class FrLinkBase;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrLinkBase, "LINKS/LINK_"); // TODO: voir aussi avec les actuateurs...
-
   class FrLink;
   TYPE_TO_NORMALIZED_PATH_PREFIX(FrLink, "LINKS/LINK_");
 
@@ -31,39 +27,6 @@ namespace frydom {
 
   class FrActuator;
   TYPE_TO_NORMALIZED_PATH_PREFIX(FrActuator, "ACTUATORS/ACTUATOR_");
-//=======
-//  // LINKS
-//
-//  class FrLinkBase;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrLinkBase, "LINKS/LINK_"); // TODO: voir aussi avec les actuateurs...
-//>>>>>>> develop
-
-//  class FrRevoluteLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrRevoluteLink, "LINKS/LINK_");
-//
-//  class FrCylindricalLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrCylindricalLink, "LINKS/LINK_");
-//
-//  class FrDOFMaskLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrDOFMaskLink, "LINKS/LINK_");
-//
-//  class FrFixedLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrFixedLink, "LINKS/LINK_");
-//
-//  class FrFreeLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrFreeLink, "LINKS/LINK_");
-//
-////  class FrLink;
-////  TYPE_TO_NORMALIZED_PATH_PREFIX(FrLink, "LINKS/LINK_");
-//
-//  class FrPrismaticLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrPrismaticLink, "LINKS/LINK_");
-//
-//  class FrPrismaticRevoluteLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrPrismaticRevoluteLink, "LINKS/LINK_");
-//
-//  class FrSphericalLink;
-//  TYPE_TO_NORMALIZED_PATH_PREFIX(FrSphericalLink, "LINKS/LINK_");
 
   // NODES
 
