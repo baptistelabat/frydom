b22a19ec768bd9e12a65d3c7cd7b420d2c44197c
Francois Rongere <francois.rongere@dice-engineering.com>
2020-02-11 18:38:18 +0100

    WIP

--- src/frydom/cable/FrLumpedMassCable.cpp
+++ src/frydom/cable/FrLumpedMassCable.cpp
@@ -41,52 +41,51 @@ namespace frydom {
     auto system = GetSystem();
 
     // Creating a catenary line to get initialization
-    auto catenary_line = FrCatenaryLine("init_line", m_startingNode, m_endingNode, m_properties, true, m_unstrainedLength, WATER); // FIXME: permettre de changer !!!
+    auto catenary_line = FrCatenaryLine("init_line", m_startingNode, m_endingNode, m_properties, true,
+                                        m_unstrainedLength, WATER); // FIXME: permettre de changer !!!
     catenary_line.Initialize();
 
     // We now have nb_elements - 1 bodies to create
 
     double diameter = m_properties->GetDiameter();
 
-    std::vector<std::shared_ptr<FrBody>> bodies;
+    std::vector<std::pair<std::shared_ptr<FrBody>, std::shared_ptr<FrNode>>> body_node_list;
     double s = 0.;
     double ds = m_unstrainedLength / m_nb_elements;
-    for (uint i=0; i<m_nb_elements-1; i++) {
+    for (uint i = 0; i < m_nb_elements - 1; i++) {
       s += ds;
 
       // Creating the body
-      auto body = system->NewBody(GetName() + "_body_" + std::to_string(i));
+      std::string body_name = GetName() + "_body_" + std::to_string(i);
+      auto body = system->NewBody(body_name);
       body->LogThis(true); // TODO: mettre a false
 
       // Set the initial position based on Catenary position
       auto position = catenary_line.GetNodePositionInWorld(s, NWU);
       body->SetPosition(position, NWU);
 
+      // Creating the body node
+      auto node = body->NewNode(body_name + "_node");
+
 //      // The mass is then set too:
 //      double mass = 2.;
 //      mass = 0.5 * (0.25 * MU_PI * diameter*diameter * length
 //      body->SetInertiaTensor(FrInertiaTensor(mass, 0., 0., 0., 0., 0., 0., {0., 0., 0.}, NWU));
 
-      bodies.push_back(body);
+      body_node_list.emplace_back(std::pair<std::shared_ptr<FrBody>, std::shared_ptr<FrNode>>(body, node));
 
     }
 
     // Now, s
     std::shared_ptr<FrNode> left_node, right_node, my_node;
     left_node = m_startingNode;
-    auto body_iter = bodies.begin();
-    for (; body_iter != bodies.end(); body_iter++) {
 
-      
-
-
-
-
-    }
-
-
-
-
+//    for ()
+//    auto iter = body_node_list.begin();
+//    for (; iter != body_node_list.end(); iter++) {
+//
+//
+//    }
 
 
   }
