1fb1b7f7295b0554adf17ac417ac1c40d929d4b5
Francois Rongere <francois.rongere@dice-engineering.com>
2020-02-20 01:55:28 +0100

    Testing the HHT solver and success in porting the time step to around 1e-2

--- tests/dev_tests/test_chrono_spring_bodies.cpp
+++ tests/dev_tests/test_chrono_spring_bodies.cpp
@@ -2,6 +2,8 @@
 // Created by frongere on 19/02/2020.
 //
 
+#include <iostream>
+
 #include "frydom/frydom.h"
 #include "chrono_irrlicht/ChIrrApp.h"
 
@@ -18,25 +20,18 @@ using namespace irr::gui;
 
 int main() {
 
-//  double diameter = 0.0332;
-//  double A = 0.25 * MU_PI * diameter * diameter;
-//  double E = 77.5e9;
-//  double rho_line = 3121;
-//  double linear_density = A * rho_line;
-//
-//  double L = 2;
-//  double K = E*A/L;
-//  double Cint = 100e6;
   double diameter = 0.0332;
   double A = 0.25 * MU_PI * diameter * diameter;
-  double E = 1e3;
+  double E = 77.5e9;
   double rho_line = 3121;
   double linear_density = A * rho_line;
 
   double L = 2;
-  double K = E*A/L;
-  double Cint = 1e5;
+  double K = E * A / L;
+  double Cint = 1e4;
 
+
+  std::cout << "Stiffness: " << K << " N/m" << std::endl;
 
   ChSystemSMC system;
 
@@ -50,15 +45,15 @@ int main() {
 
   auto body1 = std::make_shared<chrono::ChBody>();
   system.AddBody(body1);
-  body1->SetMass(10);
-  ChVector<> position(0, 0, 0);
+  body1->SetMass(140);
+  ChVector<> position(0, -5.5, 0);
   body1->SetPos(position);
 //  auto body1_anchor = std::make_shared<ChMarker>();
 //  body1->AddMarker(body1_anchor);
 //  body1_anchor->Impose_Abs_Coord(ChCoordsys<>(ChVector<>(2, 0, 0)));
   auto sphere = std::make_shared<ChSphereShape>();
-  sphere->GetSphereGeometry().center = position;
-  sphere->GetSphereGeometry().rad = 10;
+  sphere->GetSphereGeometry().center = ChVector<>();
+  sphere->GetSphereGeometry().rad = 1;
   body1->AddAsset(sphere);
 
 
@@ -66,40 +61,58 @@ int main() {
   spring1->Set_SpringRestLength(2);
   spring1->Set_SpringK(K);
   spring1->Set_SpringR(Cint);
-  spring1->Initialize(world_body, body1, true, ChVector<>(), ChVector<>(), false, 2.);
+  spring1->Initialize(world_body,
+                      body1,
+                      true,
+                      ChVector<>(),
+                      ChVector<>(),
+                      false,
+                      5.);
   system.AddLink(spring1);
-
 
 
   ChIrrApp application(&system);
 
 //  application.AddTypicalSky();
   application.AddTypicalLights();
-  application.AddTypicalCamera(core::vector3df(0, 40, -60));
+  application.AddTypicalCamera(core::vector3df(0, 8, -12));
 
   application.AssetBindAll();
   application.AssetUpdateAll();
-  application.SetTimestep(1e-3);
+  application.SetTimestep(1e-2);
 
+
+//  system.SetSolverType(ChSolver::Type::BARZILAIBORWEIN);
+//  system.SetTimestepperType(ChTimestepper::Type::RUNGEKUTTA45);
+//  system.SetTimestepperType(ChTimestepper::Type::NEWMARK);
+
+//  system.SetTimestepperType(ChTimestepper::Type::HEUN);
+
+  system.SetTimestepperType(ChTimestepper::Type::HHT);
+  auto integrator = std::dynamic_pointer_cast<chrono::ChTimestepperHHT>(system.GetTimestepper());
+  application.SetTimestep(5e-3); // Pour initialiser le pas de temps
+  integrator->SetAlpha(-0.28); // Min is -0.33333333 == max damping
+  integrator->SetMaxiters(8);
+  integrator->SetAbsTolerances(5e-5, 1.8);
+  integrator->SetMode(ChTimestepperHHT::POSITION);
+  integrator->SetModifiedNewton(false);
+  integrator->SetScaling(true);
+//  integrator->SetStepControl(false);
+//  integrator->SetVerbose(true);
+
+
+  auto body_pos = body1->GetPos();
+  std::cout << "Time: " << system.GetChTime() << "\tbody pos: " << body_pos.y() << std::endl;
   while (application.GetDevice()->run()) {
-    application.BeginScene(true, true, SColor(255,140,161,192));
+    application.BeginScene(true, true, SColor(255, 140, 161, 192));
     application.DrawAll();
     application.DoStep();
     application.EndScene();
 
-    auto body_pos = body1->GetPos();
-//    std::cout << body_pos.z() << std::endl;
+    body_pos = body1->GetPos();
+    std::cout << "Time: " << system.GetChTime() << "\tbody pos: " << body_pos.y() << std::endl;
 
   }
-
-
-
-
-
-
-
-
-
 
 
 }
