2e80737e016a65bfe2305a88b73a48c563b98507
Francois Rongere <francois.rongere@dice-engineering.com>
2020-02-24 19:01:55 +0100

    Keep playing around with solvers for stability and speed

--- tests/dev_tests/test_chrono_spring_bodies.cpp
+++ tests/dev_tests/test_chrono_spring_bodies.cpp
@@ -18,39 +18,39 @@ using namespace irr::video;
 using namespace irr::io;
 using namespace irr::gui;
 
-int main() {
 
-  double diameter = 0.0332;
-  double A = 0.25 * MU_PI * diameter * diameter;
-  double E = 77.5e9;
-  double rho_line = 3121;
-  double linear_density = A * rho_line;
+double diameter = 0.0332;
+double A = 0.25 * MU_PI * diameter * diameter;
+double E = 77.5e9;
+double rho_line = 3121;
+double linear_density = A * rho_line;
 
-  double L = 5;
-  double K = E * A / L;
-  double Cint = 1e2;
-  double damping = Cint * A;
-
-  std::cout << "Stiffness: " << K << " N/m" << std::endl;
-
-  ChSystemSMC system;
-
-  auto world_body = std::make_shared<chrono::ChBody>();
-  system.AddBody(world_body);
-  world_body->SetBodyFixed(true);
-//  auto anchor = std::make_shared<ChMarker>();
-//  world_body->AddMarker(anchor);
-//  anchor->Impose_Abs_Coord(ChCoordsys<>(ChVector<>(0, 0, 0)));
+//double L = 3.33;
+//double K = E * A / L;
+double Cint = 1e5;
+double damping = Cint * A;
+//double damping = Cint / A;
 
 
-  auto body1 = std::make_shared<chrono::ChBody>();
-  system.AddBody(body1);
-  body1->SetMass(140);
-  ChVector<> position(0, -L+0.5, 0);
-  body1->SetPos(position);
 
-//  body1->SetMaxSpeed(2); // info: Permet de stabiliser de maniere artificielle !!!
-  body1->SetLimitSpeed(true);
+std::shared_ptr<ChBody> NewElement(std::shared_ptr<ChBody> prev_body, double L) {
+
+  auto system = prev_body->GetSystem();
+
+  auto new_body = std::make_shared<chrono::ChBody>();
+  system->AddBody(new_body);
+
+  double lumped_mass = linear_density * L;
+
+//  lumped_mass = 50;
+//  std::cout << lumped_mass << std::endl;
+
+  new_body->SetMass(lumped_mass);
+  ChVector<> position(0.0, -L*1.000, 0);
+  new_body->SetPos(prev_body->GetPos() + position);
+
+  new_body->SetMaxSpeed(10); // info: Permet de stabiliser de maniere artificielle !!!
+  new_body->SetLimitSpeed(true);
   // TODO: ca stabilise bien et permet de monter en dt. L'idee serait alors a chaque pas de temps de prendre
   // la vitesse du fairlead et de regler le maxspeed des noeuds LM a x% de sa vitesse...
 
@@ -60,58 +60,132 @@ int main() {
 //  body1_anchor->Impose_Abs_Coord(ChCoordsys<>(ChVector<>(2, 0, 0)));
   auto sphere = std::make_shared<ChSphereShape>();
   sphere->GetSphereGeometry().center = ChVector<>(); // Attention, la position est relative au corps ici...
-  sphere->GetSphereGeometry().rad = 1;
-  body1->AddAsset(sphere);
+  sphere->GetSphereGeometry().rad = 0.5;
+  new_body->AddAsset(sphere);
 
 
   auto spring1 = std::make_shared<ChLinkSpring>();
   spring1->Set_SpringRestLength(L);
-  spring1->Set_SpringK(K);
+  spring1->Set_SpringK( E*A / L);
   spring1->Set_SpringR(damping);
-  spring1->Initialize(world_body, // TODO: utiliser les marker a la place comme fait dans LM...
-                      body1,
+  spring1->Initialize(prev_body, // TODO: utiliser les marker a la place comme fait dans LM...
+                      new_body,
                       true,
                       ChVector<>(),
                       ChVector<>(),
                       false,
                       L);
-  system.AddLink(spring1);
+  system->AddLink(spring1);
+
+  return new_body;
+
+}
 
 
+
+int main() {
+
+  ChSystemSMC system;
+
+  auto world_body = std::make_shared<chrono::ChBody>();
+  system.AddBody(world_body);
+  world_body->SetBodyFixed(true);
+
+  double l = 5;
+
+  auto body1 = NewElement(world_body, l);
+  auto body2 = NewElement(body1, l);
+  auto body3 = NewElement(body2, l);
+  auto body4 = NewElement(body3, l);
+  auto body5 = NewElement(body4, l);
+  auto body6 = NewElement(body5, l);
+  auto body7 = NewElement(body6, l);
+  auto body8 = NewElement(body7, l);
+  auto body9 = NewElement(body8, l);
+  auto body10 = NewElement(body9, l);
+  auto body11 = NewElement(body10, l);
+  auto body12 = NewElement(body11, l);
+  auto body13 = NewElement(body12, l);
+  auto body14 = NewElement(body13, l);
+  auto body15 = NewElement(body14, l);
+  auto body16 = NewElement(body15, l);
+  auto body17 = NewElement(body16, l);
+  auto body18 = NewElement(body17, l);
+  auto body19 = NewElement(body18, l);
+  auto body20 = NewElement(body19, l);
+  auto body21 = NewElement(body20, l);
+
+
+  #define IRRLICHT
+
+
+#ifdef IRRLICHT
+  //  Irrlicht
   ChIrrApp application(&system);
 
-//  application.AddTypicalSky();
   application.AddTypicalLights();
-  application.AddTypicalCamera(core::vector3df(0, 8, -12));
+  application.AddTypicalCamera(core::vector3df(0, 50, -70));
 
   application.AssetBindAll();
   application.AssetUpdateAll();
-//  application.SetTimestep(9e-3);
-  application.SetTimestep(1e-2);
+
+#endif
+
+  double timestep = 1e-3;
 
 
+//  application.SetTimestep(timestep);
+
+
+
+
+
+
+// Clairement, la liaison spring n'introduit aucune contrainte. Le solveur n'est donc pas utilisé !!!
 //  system.SetSolverType(ChSolver::Type::BARZILAIBORWEIN);
-  system.SetTimestepperType(ChTimestepper::Type::RUNGEKUTTA45);
-//  system.SetTimestepperType(ChTimestepper::Type::NEWMARK);
+//  system.SetSolverType(ChSolver::Type::MINRES);
+//  system.GetSolver()->SetVerbose(true);
+
+
+//  system.SetTimestepperType(ChTimestepper::Type::EULER_IMPLICIT_LINEARIZED);
+//  system.SetTimestepperType(ChTimestepper::Type::EULER_IMPLICIT_PROJECTED);
+//  system.SetTimestepperType(ChTimestepper::Type::RUNGEKUTTA45);
+
 
 //  system.SetTimestepperType(ChTimestepper::Type::HEUN);
 
 
+//  system.SetTimestepperType(ChTimestepper::Type::NEWMARK);
+//  auto integrator = std::dynamic_pointer_cast<chrono::ChTimestepperNewmark>(system.GetTimestepper());
+//  integrator->SetGammaBeta(0.75, 0.25);
 
-//  system.SetTimestepperType(ChTimestepper::Type::HHT);
-//  auto integrator = std::dynamic_pointer_cast<chrono::ChTimestepperHHT>(system.GetTimestepper());
-//  application.SetTimestep(1e-3); // Pour initialiser le pas de temps
-//  integrator->SetAlpha(-0.2); // Min is -0.33333333 == max damping
+
+
+
+  system.SetTimestepperType(ChTimestepper::Type::HHT);
+  auto integrator = std::dynamic_pointer_cast<chrono::ChTimestepperHHT>(system.GetTimestepper());
+  #ifdef IRRLICHT
+  application.SetTimestep(timestep); // Pour initialiser le pas de temps
+  #endif
+  integrator->SetAlpha(-0.25); // Min is -0.33333333 == max damping
 //  integrator->SetMaxiters(8);
-//  integrator->SetAbsTolerances(5e-5, 1.8);
+  integrator->SetMaxiters(30);
+  integrator->SetAbsTolerances(5e-5, 1.8);
+//  integrator->SetAbsTolerances(1e-2, 1.8);
 //  integrator->SetMode(ChTimestepperHHT::POSITION);
+  integrator->SetMode(ChTimestepperHHT::POSITION);
 //  integrator->SetModifiedNewton(false);
-//  integrator->SetScaling(true);
-////  integrator->SetStepControl(false);
-////  integrator->SetVerbose(true);
+  integrator->SetModifiedNewton(true);
+
+  integrator->SetScaling(true);
+  integrator->SetStepControl(true);
+//  integrator->SetVerbose(true);
 
 
   auto body_pos = body1->GetPos();
+
+
+#ifdef IRRLICHT
   std::cout << "Time: " << system.GetChTime() << "\tbody pos: " << body_pos.y() << std::endl;
   while (application.GetDevice()->run()) {
     application.BeginScene(true, true, SColor(255, 140, 161, 192));
@@ -120,10 +194,23 @@ int main() {
     application.EndScene();
 
     body_pos = body1->GetPos();
-    std::cout << "Time: " << system.GetChTime() << "\tbody pos: " << body_pos.y() << std::endl;
-    std::cout << spring1->Get_SpringReact() << std::endl;
+    std::cout << "Time: " << system.GetChTime() << "\tbody pos: " << body_pos.y() << "\tVel: " << body1->GetPos_dt().Length() << std::endl;
+//    std::cout << spring1->Get_SpringReact() << std::endl;
+//    std::cout << system.GetTimerSolver() << std::endl;
+//    std::cout << system.GetSolver()
 
   }
+#else
+
+  while (true) {
+    system.DoStepDynamics(timestep);
+
+    body_pos = body1->GetPos();
+    std::cout << "Time: " << system.GetChTime() << "\tbody pos: " << body_pos.y() << "\tVel: " << body1->GetPos_dt().Length() << std::endl;
+
+  }
+
+#endif
 
 
 }
